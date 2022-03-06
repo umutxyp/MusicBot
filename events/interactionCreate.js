fd9ffcff34fa02aae03a6c9807635e13217b8399
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-03-06 15:42:10 +0300

    added new options.
    Added save buttons with new embed and song link and added button refresh for song duration.

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,17 +1,47 @@
+const Discord = require('discord.js');
 module.exports = (client, int) => {
     if (!int.isButton()) return;
 
     const queue = client.player.getQueue(int.guildId);
-
     switch (int.customId) {
         case 'saveTrack': {
           if (!queue || !queue.playing) return int.reply({ content: `No music currently playing. ❌`, ephemeral: true, components: [] });
 
-            int.member.send(`**Track Saved: \`${queue.current.title}\` | Posted by \`${queue.current.author}\`, Saved Server: \`${int.member.guild.name}\` ✅**`).then(() => {
+          const embed = new Discord.MessageEmbed()
+          .setColor('BLUE')
+          .setTitle(client.user.username + " - Save Track")
+          .setThumbnail(client.user.displayAvatarURL())
+          .addField(`Track`, `\`${queue.current.title}\``)
+          .addField(`Duration`, `\`${queue.current.duration}\``)
+          .addField(`URL`, `${queue.current.url}`)
+          .addField(`Saved Server`, `\`${int.guild.name}\``)
+          .addField(`Requested By`, `${queue.current.requestedBy}`)
+          .setTimestamp()
+          .setFooter({ text: 'Music Bot Commands - by Umut Bayraktar ❤️', iconURL: int.user.displayAvatarURL({ dynamic: true }) });
+          int.member.send({ embeds: [embed] }).then(() => {
                 return int.reply({ content: `I sent you the name of the music in a private message ✅`, ephemeral: true, components: [] });
             }).catch(error => {
                 return int.reply({ content: `I can't send you a private message. ❌`, ephemeral: true, components: [] });
             });
+        }
+        break
+        case 'time': {
+            if (!queue || !queue.playing) return int.reply({ content: `No music currently playing. ❌`, ephemeral: true, components: [] });
+
+            const progress = queue.createProgressBar();
+            const timestamp = queue.getPlayerTimestamp();
+    
+            if (timestamp.progress == 'Infinity') return int.message.edit({ content: `This song is live streaming, no duration data to display. 🎧` });
+    
+            const embed = new Discord.MessageEmbed()
+            .setColor('BLUE')
+            .setTitle(queue.current.title)
+            .setThumbnail(client.user.displayAvatarURL())
+            .setTimestamp()
+            .setDescription(`${progress} (**${timestamp.progress}**%)`)
+            .setFooter({ text: 'Music Bot Commands - by Umut Bayraktar ❤️', iconURL: int.user.displayAvatarURL({ dynamic: true }) });
+            int.message.edit({ embeds: [embed] });
+            int.reply({ content: `**✅ Success:** Time data updated. `, ephemeral: true});
         }
     }
 };
