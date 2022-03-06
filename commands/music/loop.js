9b3aa0eb096a5cd8c9f064a1299be106158b9f65
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-03-06 15:45:38 +0300

    v13 updates.

--- commands/music/loop.js
+++ commands/music/loop.js
@@ -10,20 +10,20 @@ module.exports = {
         const queue = client.player.getQueue(message.guild.id);
 
  
-if (!queue || !queue.playing) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. ❌` });
 
         if (args.join('').toLowerCase() === 'queue') {
-            if (queue.repeatMode === 1) return message.channel.send(`${message.author}, You should disable loop mode of existing music first **(${client.config.px}loop)** ❌`);
+            if (queue.repeatMode === 1) return message.channel.send({ content: `${message.author}, You should disable loop mode of existing music first **(${client.config.px}loop)** ❌` });
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
 
-            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? 'Inactive' : 'Active'}**, The whole sequence will repeat non-stop 🔁` : `${message.author}, Something went wrong. ❌`);
+            return message.channel.send({ content: success ? `Loop Mode: **${queue.repeatMode === 0 ? 'Inactive' : 'Active'}**, The whole sequence will repeat non-stop 🔁` : `${message.author}, Something went wrong. ❌` });
         } else {
-            if (queue.repeatMode === 2) return message.channel.send(`${message.author}, In Loop mode you must disable existing queue first **(${client.config.px}loop queue)** ❌`);
+            if (queue.repeatMode === 2) return message.channel.send({ content: `${message.author}, In Loop mode you must disable existing queue first **(${client.config.px}loop queue)** ❌` });
 
             const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.TRACK : QueueRepeatMode.OFF);
 
-            return message.channel.send(success ? `Loop Mode: **${queue.repeatMode === 0 ? 'Inactive' : 'Active'}**, Current music will be repeated non-stop (all music in the list **${client.config.px}loop queue**  You can repeat it with the option.) 🔂` : `${message.author}, Something went wrong ❌`);
+            return message.channel.send({ content: success ? `Loop Mode: **${queue.repeatMode === 0 ? 'Inactive' : 'Active'}**, Current music will be repeated non-stop (all music in the list **${client.config.px}loop queue**  You can repeat it with the option.) 🔂` : `${message.author}, Something went wrong ❌` });
 };
     },
 };
