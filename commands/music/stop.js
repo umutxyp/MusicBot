6a32f5e99d131e0f756fb36da97885446f6a2f97
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-03-06 15:49:41 +0300

    v13 updates.

--- commands/music/stop.js
+++ commands/music/stop.js
@@ -7,10 +7,10 @@ module.exports = {
     execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+        if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. ❌` });
 
         queue.destroy();
 
-        message.channel.send(`The music playing on this server has been turned off, see you next time ✅`);
+        message.channel.send({ content: `The music playing on this server has been turned off, see you next time ✅` });
     },
 };
