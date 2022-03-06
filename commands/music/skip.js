46d42df7f70664e80b37c57b37fd26ae0106ea9b
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-03-06 15:49:22 +0300

    v13 updates.

--- commands/music/skip.js
+++ commands/music/skip.js
@@ -7,10 +7,10 @@ module.exports = {
     execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
  
-        if (!queue || !queue.playing) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+        if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. ❌` });
 
         const success = queue.skip();
 
-        return message.channel.send(success ? `**${queue.current.title}**, Skipped song ✅` : `${message.author}, Something went wrong ❌`);
+        return message.channel.send({ content: success ? `**${queue.current.title}**, Skipped song ✅` : `${message.author}, Something went wrong ❌` });
     },
 };
