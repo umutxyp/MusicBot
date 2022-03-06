e12ef087b3faf3f9060ccf8fa1341abb3ecb19a6
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-03-06 15:48:43 +0300

    v13 updates and new options.

--- commands/music/save.js
+++ commands/music/save.js
@@ -1,3 +1,4 @@
+const Discord = require('discord.js');
 module.exports = {
     name: 'save',
     aliases: [],
@@ -7,12 +8,23 @@ module.exports = {
     async execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
-  if (!queue || !queue.playing) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+  if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. ❌` });
 
-        message.author.send(`Registered track: **${queue.current.title}** | ${queue.current.author}, Saved server: **${message.guild.name}** ✅`) .then(() => {
-            message.channel.send(`I sent the name of the music via private message. ✅`);
+  const embed = new Discord.MessageEmbed()
+  .setColor('BLUE')
+  .setTitle(client.user.username + " - Save Track")
+  .setThumbnail(client.user.displayAvatarURL())
+  .addField(`Track`, `\`${queue.current.title}\``)
+  .addField(`Duration`, `\`${queue.current.duration}\``)
+  .addField(`URL`, `${queue.current.url}`)
+  .addField(`Saved Server`, `\`${message.guild.name}\``)
+  .addField(`Requested By`, `${queue.current.requestedBy}`)
+  .setTimestamp()
+  .setFooter({ text: 'Music Bot Commands - by Umut Bayraktar ❤️', iconURL: message.author.avatarURL({ dynamic: true }) });
+  message.author.send({ embeds: [embed] }).then(() => {
+            message.channel.send({ content: `I sent the name of the music via private message. ✅` });
         }).catch(error => {
-            message.channel.send(`${message.author}, Unable to send you private message. ❌`);
+            message.channel.send({ content: `${message.author}, Unable to send you private message. ❌` });
         });
     },
 };
