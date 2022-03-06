c85366f5cc0fd2811718080af3e133286b6fda60
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-03-06 15:46:28 +0300

    added button system auto updater and v13 updates.

--- commands/music/nowplaying.js
+++ commands/music/nowplaying.js
@@ -9,13 +9,13 @@ module.exports = {
     execute(client, message) {
         const queue = client.player.getQueue(message.guild.id);
 
- if (!queue || !queue.playing) return message.channel.send(`${message.author}, There is no music currently playing!. ❌`);
+ if (!queue || !queue.playing) return message.channel.send({ content: `${message.author}, There is no music currently playing!. ❌` });
 
         const track = queue.current;
 
         const embed = new MessageEmbed();
 
-        embed.setColor('RED');
+        embed.setColor('BLUE');
         embed.setThumbnail(track.thumbnail);
         embed.setTitle(track.title)
 
@@ -24,10 +24,10 @@ module.exports = {
         const timestamp = queue.getPlayerTimestamp();
 const trackDuration = timestamp.progress == 'Forever' ? 'Endless (Live)' : track.duration;
 
-        embed.setDescription(`Audio **%${queue.volume}**\nDuration **${trackDuration}**\nLoop Mode **${methods[queue.repeatMode]}**\n${track. requestedBy}`);
+        embed.setDescription(`Audio **%${queue.volume}**\nDuration **${trackDuration}**\nURL: ${track.url}\nLoop Mode **${methods[queue.repeatMode]}**\n${track. requestedBy}`);
 
         embed.setTimestamp();
-        embed.setFooter('Edited by Umut Bayraktar ❤️', message.author.avatarURL({ dynamic: true }));
+        embed.setFooter({ text: 'by Umut Bayraktar ❤️', iconURL: message.author.avatarURL({ dynamic: true }) });
 
         const saveButton = new MessageButton();
 
