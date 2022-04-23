02ebc4b4aeddeb368f3b8fc50a09bb5de9205678
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-04-23 23:47:41 +0300

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,10 @@ module.exports = {
         },
             
         selfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
-        selfDeafTimeout: 5000, //1000 = 1 second
+        selfDeafTimeout:{
+                status: true, //If this variable is "true", the bot will leave the channel a certain time after the music ends.
+                time: 5000, //1000 = 1 second
+        },
             
         maxVol: 100, //You can specify the maximum volume level.
         loopMessage: false, //Please don't touch
