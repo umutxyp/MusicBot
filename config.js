169c316b8a93acedec07f97c927401a30a33a2a7
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-04-24 00:34:04 +0300

    voice channel settings added
    Added settings to leave the audio channel after a certain time or not leave at all

--- config.js
+++ config.js
@@ -3,26 +3,31 @@ module.exports = {
         px: '!',
         playing: 'Umut Bayraktar ❤️ Music Bot',
 
-    opt: {
+ opt: {
         DJ: {
             enabled: false, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
             
-        selfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
-        voiceChannelLeave:{
-                status: true, //If this variable is "true", the bot will leave the channel a certain time after the music ends.
-                time: 5000, //1000 = 1 second
-        },
-            
+        voiceConfig: {
+            leaveOnEnd: false, //If this variable is "true", the bot will leave the channel the music ends.
+            autoSelfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
+
+            leaveOnTimer:{ //The leaveOnEnd variable must be "false" to use this system.
+                status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
+                time: 10000, //1000 = 1 second
+            }
+        }, 
+
         maxVol: 100, //You can specify the maximum volume level.
-        loopMessage: false, //Please don't touch
+        loopMessage: false,
+
         discordPlayer: {
             ytdlOptions: {
                 quality: 'highestaudio', //Please don't touch
                 highWaterMark: 1 << 25 //Please don't touch
             }
+            }
         }
-    }
-};
+}
