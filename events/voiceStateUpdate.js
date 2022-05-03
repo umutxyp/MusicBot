225cbaf92e80b4004188755e02c4176c5650e3e3
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-03 13:00:27 +0300

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,9 +1,9 @@
-module.exports = async (client, _, newState) {
-  const player = client.player
-  if(newState.channelId !== null) return;
-  if(newState.member.user.id !== client.user.id) return;
-
-  const queue = player.getQueue(newState.guild.id)
-  queue.metadata.send({ content: 'Someone from the audio channel Im connected to kicked me out, the whole playlist has been cleared! ❌' });
-  player.deleteQueue(queue.metadata.guild.id);
-}
+module.exports = async (client, newState) => {
+    const player = client.player
+    if(newState.channelId !== null) return;
+    if(newState.member.user.id !== client.user.id) return;
+  
+    const queue = player.getQueue(newState.guild.id)
+    queue.metadata.send({ content: 'Someone from the audio channel Im connected to kicked me out, the whole playlist has been cleared! ❌' });
+    player.deleteQueue(queue.metadata.guild.id);
+  }
