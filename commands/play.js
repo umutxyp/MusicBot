5074b957266ab0891fce15649b2718493b6b822f
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-04 16:32:19 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -35,7 +35,7 @@ module.exports = {
             return interaction.reply({ content: `I can't join audio channel. ❌`, ephemeral: true });
         }
 
-        await interaction.reply({ content: `Your ${res.playlist ? 'Playlist' : 'Track'} Loading... 🎧` });
+        await interaction.channel.send({ content: `Your ${res.playlist ? 'Playlist' : 'Track'} Loading... 🎧` });
          
         res.playlist ? queue.addTracks(res.tracks) : queue.addTrack(res.tracks[0]);
         if (!queue.playing) await queue.play();
