690be8a03f96c201ce196049091c7f18d185f104
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-04 16:17:56 +0300

    slash commands update.

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { Player } = require('discord-player');
 const { Client, Intents, Collection } = require('discord.js');
-const { readdirSync } = require('fs');
+const fs = require('fs');
 
 let client = new Client({
     intents: [
@@ -13,39 +13,55 @@ let client = new Client({
 
 client.config = require('./config');
 client.player = new Player(client, client.config.opt.discordPlayer);
-client.commands = new Collection();
 const player = client.player
 
-const events = readdirSync('./events/').filter(file => file.endsWith('.js'));
-for (const file of events) {
-    const event = require(`./events/${file}`);
-    console.log(`-> Loaded event ${file.split('.')[0]}`);
-    client.on(file.split('.')[0], event.bind(null, client));
-    delete require.cache[require.resolve(`./events/${file}`)];
-};
-console.log(`-> Loaded commands...`);
-readdirSync('./commands/').forEach(dirs => {
-    const commands = readdirSync(`./commands/${dirs}`).filter(files => files.endsWith('.js'));
-    for (const file of commands) {
-        const command = require(`./commands/${dirs}/${file}`);
-        console.log(`${command.name.toLowerCase()} Load Command!`);
-        client.commands.set(command.name.toLowerCase(), command);
-        delete require.cache[require.resolve(`./commands/${dirs}/${file}`)];
-    };
+const synchronizeSlashCommands = require('discord-sync-commands-v14');
+
+client.commands = new Collection();
+fs.readdir("./commands/", (_err, files) => {
+    files.forEach((file) => {
+        if (!file.endsWith(".js")) return;
+        let props = require(`./commands/${file}`);
+        let commandName = file.split(".")[0];
+        client.commands.set(commandName, {
+            name: commandName,
+            ...props
+        });
+        console.log(`👌 Yüklendi slash komutu: ${commandName}`);
+    });
+    synchronizeSlashCommands(client, client.commands.map((c) => ({
+        name: c.name,
+        description: c.description,
+        options: c.options,
+        type: 'CHAT_INPUT'
+    })), {
+        debug: false
+    });
+});
+
+fs.readdir("./events", (_err, files) => {
+  files.forEach((file) => {
+      if (!file.endsWith(".js")) return;
+      const event = require(`./events/${file}`);
+      let eventName = file.split(".")[0];
+      console.log(`👌 Event Yüklendi: ${eventName}`);
+      client.on(eventName, event.bind(null, client));
+      delete require.cache[require.resolve(`./events/${file}`)];
+  });
 });
 
 
 player.on('trackStart', (queue, track) => {
     if (!client.config.opt.loopMessage && queue.repeatMode !== 0) return;
-    queue.metadata.send({ content: `🎵 Music started playing: **${track.title}** -> Channel: **${queue.connection.channel.name}** 🎧` });
+    queue.metadata.send({ content: `🎵 Music started playing: **${track.title}** -> Channel: **${queue.connection.channel.name}** 🎧` }).catch(e => { })
 });
 
 player.on('trackAdd', (queue, track) => {
-    queue.metadata.send({ content: `**${track.title}** added to playlist. ✅` });
+    queue.metadata.send({ content: `**${track.title}** added to playlist. ✅` }).catch(e => { })
 });
 
 player.on('channelEmpty', (queue) => {
-    queue.metadata.send({ content: 'I left the audio channel because there is no one on my audio channel. ❌' });
+    queue.metadata.send({ content: 'I left the audio channel because there is no one on my audio channel. ❌' }).catch(e => { })
 });
 
 player.on('queueEnd', (queue) => {
@@ -54,7 +70,7 @@ player.on('queueEnd', (queue) => {
             if(queue.connection) queue.connection.disconnect();
         }, client.config.opt.voiceConfig.leaveOnTimer.time);
     }
-    queue.metadata.send({ content: 'All play queue finished, I think you can listen to some more music. ✅' })
+    queue.metadata.send({ content: 'All play queue finished, I think you can listen to some more music. ✅' }).catch(e => { })
 });
 
 
