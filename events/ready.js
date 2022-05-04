f1ebacd6033b18df88610eeb1b6ff2058fafee29
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-04 16:19:15 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,5 +1,11 @@
 module.exports = async (client) => {
     console.log(`${client.user.username} Login!`);
-
-    client.user.setActivity(client.config.playing);
+    client.user.setActivity(client.config.playing, {
+        type: "LISTENING"
+      })
+      setInterval(() => {
+        client.user.setActivity(client.config.playing, {
+            type: "LISTENING"
+          })
+      }, 600000)
 };
