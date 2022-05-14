be2c45229a87e56404e6e683773e79f022db4133
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-14 15:50:14 +0300

    Update main.js

--- main.js
+++ main.js
@@ -12,6 +12,7 @@ let client = new Client({
 });
 
 client.db = require("orio.db")
+client.db.deleteAll()
 client.config = require('./config');
 client.player = new Player(client, client.config.opt.discordPlayer);
 const player = client.player
