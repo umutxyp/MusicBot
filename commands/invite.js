a6b14504198740f2ce7030fccc4d6a5f0408ba8a
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-23 18:48:03 +0300

    Update invite.js

--- commands/invite.js
+++ commands/invite.js
@@ -11,7 +11,7 @@ module.exports = {
         .setTitle(client.user.username)
         .setThumbnail(client.user.displayAvatarURL())
         .setDescription("It's time to listen to music on your discord server with a completely free and advanced interface. Music bot Astra that supports playing music on many platforms that will make your server feel special")
-        .addField("Invite Bot", `**[Add Me](https://bit.ly/3LIb7Zv) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astramusic.vercel.app) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
+        .addField("Invite Bot", `**[Add Me](https://bit.ly/3LIb7Zv) | [Vote](https://top.gg/bot/964995884234448987/vote) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astramusic.vercel.app) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
         .setTimestamp()
         .setFooter({ text: 'Music Bot - by Umut Bayraktar ❤️', iconURL:interaction.user.displayAvatarURL({ dynamic: true }) })
         interaction.reply({ embeds: [embed] }).catch(e => { })
