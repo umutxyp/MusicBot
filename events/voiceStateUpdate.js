4af696a9b207e60feb408b9a50f3520ece6525c9
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-28 19:42:49 +0300

    Bug fix.

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -3,10 +3,12 @@ module.exports = async (client, oldState, newState) => {
     if(oldState.channelId && !newState.channelId){
    const queue = client.player?.getQueue(newState.guild.id)
    if(queue){
+     if(queue.playing){
      if(queue.metadata){
     queue.metadata.send({ content: 'Someone from the audio channel Im connected to kicked me out, the whole playlist has been cleared! ❌' }).catch(e => {})
      }
     client.player?.deleteQueue(queue.metadata.guild.id)
+}
 }
     }
   }
