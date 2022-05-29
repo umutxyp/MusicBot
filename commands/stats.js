5db6087a18307949c10a1f7d5fcbfa91e415190f
Umut Bayraktar <69085038+1umutda@users.noreply.github.com>
2022-05-29 11:28:17 +0300

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -23,8 +23,8 @@ module.exports = {
                 .setDescription(`> **Guilds: \`${client.guilds.cache.size}\`**
 > **Users: \`${Math.ceil(client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString("tr-TR"))}.000\`**
 > **Channels: \`${client.channels.cache.size}\`**`)
-.addField("Invite Bot", `**[Add Me](https://bit.ly/3LIb7Zv) | [Vote](https://top.gg/bot/964995884234448987/vote) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astramusic.vercel.app) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
-interaction.reply({embeds:[embed], components:[button]}).then(async Message => {
+.addField("Invite Bot", `**[Add Me](https://bit.ly/3PHDjyC) | [Vote](https://bit.ly/3LYzaDe) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astrabot.vercel.app/) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
+                      interaction.reply({embeds:[embed], components:[button]}).then(async Message => {
                     
                     const filter = i =>  i.user.id === interaction.user.id
                     let col = await interaction.channel.createMessageComponentCollector({filter, time: 180000 });
@@ -43,8 +43,8 @@ interaction.reply({embeds:[embed], components:[button]}).then(async Message => {
                         .setDescription(`> **Guilds: \`${client.guilds.cache.size}\`**
 > **Users: \`${Math.ceil(client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString("tr-TR"))}.000\`**
 > **Channels: \`${client.channels.cache.size}\`**`)
-.addField("Invite Bot", `**[Add Me](https://bit.ly/3LIb7Zv) | [Vote](https://top.gg/bot/964995884234448987/vote) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astramusic.vercel.app) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
-await interaction.editReply({embeds: [embedd]}).catch(e => { });
+.addField("Invite Bot", `**[Add Me](https://bit.ly/3PHDjyC) | [Vote](https://bit.ly/3LYzaDe) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astrabot.vercel.app/) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
+                      await interaction.editReply({embeds: [embedd]}).catch(e => { });
                         button.reply({content: "> **✅ Success:** Bot statistics updated!", ephemeral: true}).catch(e => { });
             
                         break
@@ -79,8 +79,8 @@ await interaction.editReply({embeds: [embedd]}).catch(e => { });
                         .setDescription(`> **Guilds: \`${client.guilds.cache.size}\`**
 > **Users: \`${Math.ceil(client.guilds.cache.reduce((a, b) => a + b.memberCount, 0).toLocaleString("tr-TR"))}.000\`**
 > **Channels: \`${client.channels.cache.size}\`**`)
-.addField("Invite Bot", `**[Add Me](https://bit.ly/3LIb7Zv) | [Vote](https://top.gg/bot/964995884234448987/vote) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astramusic.vercel.app) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
-await interaction.editReply({embeds: [embedd], components:[button]}).catch(e => { });
+.addField("Invite Bot", `**[Add Me](https://bit.ly/3PHDjyC) | [Vote](https://bit.ly/3LYzaDe) | [Support](https://discord.gg/ST89uArTdh) | [Website](https://astrabot.vercel.app/) | [Source Code](https://github.com/1umutda/MusicBot)**` ,true)
+                      await interaction.editReply({embeds: [embedd], components:[button]}).catch(e => { });
                     })
                 }).catch(e => { });
     },
