b91c9b2925a02d6b7990108b74cb4cfdb1221dc1
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:28:09 +0300

    v14 updates

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,22 @@
+const config = require("../config.js");
 module.exports = async (client) => {
-    console.log(`${client.user.username} Login!`);
-    client.user.setActivity(client.config.playing, {
-        type: "LISTENING"
-      })
-      setInterval(() => {
-        client.user.setActivity(client.config.playing, {
-            type: "LISTENING"
-          })
-      }, 600000)
-};
+
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const rest = new REST({ version: "10" }).setToken(config.TOKEN);
+(async () => {
+try {
+await rest.put(Routes.applicationCommands(client.user.id), {
+body: await client.commands,
+});
+console.log("Successfully reloaded application [/] commands.");
+} catch (err) {
+console.log("Error reloading application [/] commands: " + err);
+}
+})();
+
+console.log(client.user.username+" successfully connected.");
+client.user.setStatus('ONLINE');
+client.user.setActivity(config.status)
+
+}
