3b8646c9a535c9e05b591f1f78c48361844c52a3
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:28:27 +0300

    v14 updates

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,95 +1,110 @@
-const { MessageEmbed } = require('discord.js');
-module.exports = async (client, int) => {
+const fs = require("fs")
+const config = require("../config.js");
+const { EmbedBuilder, InteractionType } = require('discord.js');
+const db = require("croxydb")
 
-if(!int.guild) return
+module.exports = async (client, interaction) => {
+if (!interaction.guild) return;
+if (interaction.type === InteractionType.ApplicationCommand){
+fs.readdir(config.commandsDir, (err, files) => {
+if (err) throw err;
+files.forEach(async (f) => {
+let props = require(`.${config.commandsDir}/${f}`);
+if (interaction.commandName.toLowerCase() === props.name.toLowerCase()) {
+try {
+if(interaction.member.permissions.has(props?.permissions || "SEND_MESSAGES")){
+const DJ = client.config.opt.DJ;
+if (props && DJ.commands.includes(interaction.commandName)) {
+let djRole = await db.get(`dj-${interaction.guild.id}`)
+if(djRole){
+const roleDJ = interaction.guild.roles.cache.get(djRole)
+if(!interaction.member.permissions.has("MANAGE_GUILD")){
+if(roleDJ){
+if(!interaction.member.roles.cache.has(roleDJ.id)){
 
-    if (int.isCommand()){
+const embed = new EmbedBuilder()
+.setColor('007fff')
+.setTitle(client.user.username)
+.setThumbnail(client.user.displayAvatarURL())
+.setDescription("You must have the <@&"+djRole+">(DJ) role set on this server to use this command. Users without this role cannot use the "+client.config.opt.DJ.commands.map(astra => '`'+astra+'`').join(", "))
+.setTimestamp()
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+return interaction.reply({ embeds: [embed], ephemeral: true}).catch(e => { })
+}}}}
+}
+if (props && props.voiceChannel) {
+if (!interaction.member.voice.channelId) return interaction.reply({ content: `You are not connected to an audio channel. ❌`, ephemeral: true}).catch(e => {})
+const guild_me = interaction.guild.members.cache.get(client.user.id);
+if (guild_me.voice.channelId){
+if(guild_me.voice.channelId !== interaction.member.voice.channelId){
+return interaction.reply({ content: `You are not on the same audio channel as me. ❌`, ephemeral: true}).catch(e => {})
+}
+}
+}
+return props.run(client, interaction);
 
-    const cmd = client.commands.get(int.commandName);
-
-    if (!cmd) return void int.reply({
-        content: `Command \`${int.commandName}\` not found.`,
-        ephemeral: true
-    }).catch(e => {})
-
-
-    const DJ = client.config.opt.DJ;
-
-        if (cmd && DJ.commands.includes(int.commandName)) {
-            let djRolefind = await client.mdb.findOne({ guildID: int.guild.id }).catch(e => { });
-            let djRole = djRolefind && djRolefind.djRole
-             if(djRole){
-          const roleDJ = int.guild.roles.cache.get(djRole)
-          if(!int.member.permissions.has("MANAGE_GUILD")){
-              if(roleDJ){
-              if(!int.member.roles.cache.has(roleDJ.id)){
-  
-              const embed = new MessageEmbed()
-              .setColor('BLUE')
-              .setTitle(client.user.username)
-              .setThumbnail(client.user.displayAvatarURL())
-              .setDescription("You must have the <@&"+djRole+">(DJ) role set on this server to use this command. Users without this role cannot use the "+client.config.opt.DJ.commands.map(astra => '`'+astra+'`').join(", "))
-              .setTimestamp()
-              .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-              return int.reply({ content: `${int.user}`, embeds: [embed], ephemeral: true}).catch(e => { })
-          }}}}
-        }
-        if (cmd && cmd.voiceChannel) {
-            if (!int.member.voice.channel) return int.reply({ content: `You are not connected to an audio channel. ❌`, embeds: [], components: [], ephemeral: true}).catch(e => {})
-            if (int.guild.me.voice.channel && int.member.voice.channel.id !== int.guild.me.voice.channel.id) return int.reply({ content: `You are not on the same audio channel as me. ❌`, embeds: [], components: [], ephemeral: true}).catch(e => {})
-        }
-        cmd.run(client, int)    
-
-    
+} else {
+return interaction.reply({ content: `Missing permission: **${props?.permissions}**`, ephemeral: true });
+}
+} catch (e) {
+console.log(e);
+return interaction.reply({ content: `Something went wrong...\n\n\`\`\`${e.message}\`\`\``, ephemeral: true });
+}
+}
+});
+});
 }
 
-    if (int.isButton()){
-        const queue = client.player.getQueue(int.guildId);
-    switch (int.customId) {
-        case 'saveTrack': {
-       if (!queue || !queue.playing){
-       return int.reply({ content: `No music currently playing. ❌`, embeds: [], components: [], ephemeral: true }).catch(e => {})
-       } else {
-          const embed = new MessageEmbed()
-          .setColor('BLUE')
-          .setTitle(client.user.username + " - Save Track")
-          .setThumbnail(client.user.displayAvatarURL())
-          .addField(`Track`, `\`${queue.current.title}\``)
-          .addField(`Duration`, `\`${queue.current.duration}\``)
-          .addField(`URL`, `${queue.current.url}`)
-          .addField(`Saved Server`, `\`${int.guild.name}\``)
-          .addField(`Requested By`, `${queue.current.requestedBy}`)
-          .setTimestamp()
-          .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-          int.member.send({ embeds: [embed] }).then(() => {
-                return int.reply({ content: `I sent you the name of the music in a private message ✅`, embeds: [], components: [], ephemeral: true}).catch(e => { })
-            }).catch(error => {
-                return int.reply({ content: `I can't send you a private message. ❌`, embeds: [], components: [], ephemeral: true}).catch(e => { })
-            });
-        }
-    }
-        break
-        case 'time': {
-            if (!queue || !queue.playing){
-                return int.reply({ content: `No music currently playing. ❌`, embeds: [], components: [], ephemeral: true }).catch(e => {})
-                } else {
-
-            const progress = queue.createProgressBar();
-            const timestamp = queue.getPlayerTimestamp();
-    
-            if (timestamp.progress == 'Infinity') return int.message.edit({ content: `This song is live streaming, no duration data to display. 🎧`, embeds: [], components: [] }).catch(e => { })
-    
-            const embed = new MessageEmbed()
-            .setColor('BLUE')
-            .setTitle(queue.current.title)
-            .setThumbnail(client.user.displayAvatarURL())
-            .setTimestamp()
-            .setDescription(`${progress} (**${timestamp.progress}**%)`)
-            .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-            int.message.edit({ embeds: [embed] }).catch(e => { })
-            int.reply({ content: `**✅ Success:** Time data updated. `, embeds: [], components: [], ephemeral: true}).catch(e => { })
-        }
-    }
-    }
+if (interaction.type === InteractionType.MessageComponent){
+const queue = client.player.getQueue(interaction.guildId);
+switch (interaction.customId) {
+case 'saveTrack': {
+if (!queue || !queue.playing){
+return interaction.reply({ content: `No music currently playing. ❌`, embeds: [], components: [], ephemeral: true }).catch(e => {})
+} else {
+const embed = new EmbedBuilder()
+.setColor('007fff')
+.setTitle(client.user.username + " - Save Track")
+.setThumbnail(client.user.displayAvatarURL())
+.addFields([
+{ name: `Track`, value: `\`${queue.current.title}\`` },
+{ name: `Duration`, value: `\`${queue.current.duration}\`` },
+{ name: `URL`, value: `${queue.current.url}` },
+{ name: `Saved Server`, value: `\`${interaction.guild.name}\`` },
+{ name: `Requested By`, value: `${queue.current.requestedBy}` }
+])
+.setTimestamp()
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+interaction.member.send({ embeds: [embed] }).then(() => {
+return interaction.reply({ content: `I sent you the name of the music in a private message ✅`, embeds: [], components: [], ephemeral: true}).catch(e => { })
+}).catch(error => {
+return interaction.reply({ content: `I can't send you a private message. ❌`, embeds: [], components: [], ephemeral: true}).catch(e => { })
+});
 }
-};
+}
+break
+case 'time': {
+if (!queue || !queue.playing){
+return interaction.reply({ content: `No music currently playing. ❌`, embeds: [], components: [], ephemeral: true }).catch(e => {})
+} else {
+
+const progress = queue.createProgressBar();
+const timestamp = queue.getPlayerTimestamp();
+
+if (timestamp.progress == 'Infinity') return interaction.message.edit({ content: `This song is live streaming, no duration data to display. 🎧`, embeds: [], components: [] }).catch(e => { })
+
+const embed = new EmbedBuilder()
+.setColor('007fff')
+.setTitle(queue.current.title)
+.setThumbnail(client.user.displayAvatarURL())
+.setTimestamp()
+.setDescription(`${progress} (**${timestamp.progress}**%)`)
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+interaction.message.edit({ embeds: [embed] }).catch(e => { })
+interaction.reply({ content: `**✅ Success:** Time data updated. `, embeds: [], components: [], ephemeral: true}).catch(e => { })
+}
+}
+}
+}
+
+}
