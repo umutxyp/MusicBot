4d58286c1acdce65fa8a50f19ecaef9f9aa162d0
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:33:12 +0300

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -1,35 +1,32 @@
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
 module.exports = {
-    description: "Indicates which minute of the music you are playing.",
-    name: 'time',
-    options: [],
-    voiceChannel: true,
-    run: async (client, interaction) => {
+name: "time",
+description: "Indicates which minute of the music you are playing.",
+permissions: "SEND_MESSAGES",
+options: [],
+run: async (client, interaction) => {
+const queue = client.player.getQueue(interaction.guild.id);
 
-        const queue = client.player.getQueue(interaction.guild.id);
+if (!queue || !queue.playing) return interaction.reply({ content: `There is no music currently playing!. ❌`, ephemeral: true }).catch(e => { })
 
-        if (!queue || !queue.playing) return interaction.reply({ content: `There is no music currently playing!. ❌`, ephemeral: true }).catch(e => { })
+const progress = queue.createProgressBar();
+const timestamp = queue.getPlayerTimestamp();
 
-        const progress = queue.createProgressBar();
-        const timestamp = queue.getPlayerTimestamp();
+if (timestamp.progress == 'Infinity') return interaction.reply({ content: `This song is live streaming, no duration data to display. 🎧`, ephemeral: true }).catch(e => { })
 
-        if (timestamp.progress == 'Infinity') return interaction.reply({ content: `This song is live streaming, no duration data to display. 🎧`, ephemeral: true }).catch(e => { })
+const saveButton = new ButtonBuilder();
+saveButton.setLabel('Update');
+saveButton.setCustomId('time');
+saveButton.setStyle(ButtonStyle.Success);
+const row = new ActionRowBuilder().addComponents(saveButton);
 
-        const saveButton = new MessageButton();
-
-        saveButton.setLabel('Update');
-        saveButton.setCustomId('time');
-        saveButton.setStyle('SUCCESS');
-
-        const row = new MessageActionRow().addComponents(saveButton);
-
-        const embed = new MessageEmbed()
-        .setColor('BLUE')
-        .setTitle(queue.current.title)
-        .setThumbnail(client.user.displayAvatarURL())
-        .setTimestamp()
-        .setDescription(`${progress} (**${timestamp.progress}**%)`)
-        .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-        interaction.reply({ embeds: [embed], components: [row]}).catch(e => { })
-    },
+const embed = new EmbedBuilder()
+.setColor('007fff')
+.setTitle(queue.current.title)
+.setThumbnail(client.user.displayAvatarURL())
+.setTimestamp()
+.setDescription(`${progress} (**${timestamp.progress}**%)`)
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+interaction.reply({ embeds: [embed], components: [row]}).catch(e => { })
+},
 };
