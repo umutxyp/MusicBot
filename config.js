81aebc1f0ece33a96f2dfbec8395bdb595ba83ac
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:29:48 +0300

    v14 updates

--- config.js
+++ config.js
@@ -1,30 +1,31 @@
 module.exports = {
-        TOKEN: '', //write your discord bot token
-        playing: '❤️ Umut Bayraktar',
-        mongoDB: "", //write your mongoDB url.
- opt: {
-        DJ: { 
-                commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
-        },
-            
-        voiceConfig: {
-            leaveOnEnd: false, //If this variable is "true", the bot will leave the channel the music ends.
-            autoSelfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
+TOKEN: '', //write your discord bot token
+status: '❤️ Umut Bayraktar', 
+commandsDir: './commands', //Please don't touch
+        
+opt: {
+DJ: { 
+commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
+},
 
-            leaveOnTimer:{ //The leaveOnEnd variable must be "false" to use this system.
-                status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
-                time: 10000, //1000 = 1 second
-            }
-        }, 
+voiceConfig: {
+leaveOnEnd: false, //If this variable is "true", the bot will leave the channel the music ends.
+autoSelfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
 
-        maxVol: 100, //You can specify the maximum volume level.
-        loopMessage: false,
+leaveOnTimer:{ //The leaveOnEnd variable must be "false" to use this system.
+status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
+time: 10000, //1000 = 1 second
+}
+}, 
 
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio', //Please don't touch
-                highWaterMark: 1 << 25 //Please don't touch
-            }
-            }
-        }
+maxVol: 100, //You can specify the maximum volume level.
+loopMessage: false,
+
+discordPlayer: {
+ytdlOptions: {
+quality: 'highestaudio', //Please don't touch
+highWaterMark: 1 << 25 //Please don't touch
+}
+}
+}
 }
