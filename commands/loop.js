48b2534511a018709f9a6320e031ff3fa750014c
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:31:42 +0300

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,66 +1,67 @@
 const { QueueRepeatMode } = require('discord-player');
-const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle, Message } = require('discord.js');
+const db = require("croxydb");
 module.exports = {
-    description: "Turns the music loop mode on or off.",
-    name: 'loop',
-    options: [],
-    voiceChannel: true,
+name: "loop",
+description: "Turns the music loop mode on or off.",
+permissions: "SEND_MESSAGES",
+options: [],
+run: async (client, interaction) => {
 
-    run: async (client, interaction) => {
-        const queue = client.player.getQueue(interaction.guild.id);
-let cmds = client.db.get("loop."+interaction.user.id+interaction.guild.id+interaction.channel.id)
+const queue = client.player.getQueue(interaction.guild.id);
+let cmds = db.get("loop."+interaction.user.id+interaction.guild.id+interaction.channel.id)
 if (!queue || !queue.playing) return interaction.reply({ content: `There is no music currently playing!. ❌`, ephemeral: true }).catch(e => { })
 if(cmds) return interaction.reply({ content: `You already have an active command here. ❌\nhttps://discord.com/channels/${interaction.guild.id}/${interaction.channel.id}`, ephemeral: true }).catch(e => { })
 
-await client.db.set("loop."+interaction.user.id+interaction.guild.id+interaction.channel.id, "active")
+await db.set("loop."+interaction.user.id+interaction.guild.id+interaction.channel.id, "active")
 
-let button = new MessageActionRow().addComponents(
-    new MessageButton()
-    .setLabel("Loop")
-    .setStyle("SUCCESS")
-    .setCustomId("loop"))
+let button = new ActionRowBuilder().addComponents(
+new ButtonBuilder()
+.setLabel("Loop")
+.setStyle(ButtonStyle.Success)
+.setCustomId("loop"))
 
-        const embed = new MessageEmbed()
-            .setColor("BLUE")
-            .setTitle('Loop System')
-            .setDescription(`**${queue.current.title}** is now looping.`)
-            .setTimestamp()
-            .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-        interaction.reply({ embeds: [embed], components:[button]}).then(async Message => {
+const embed = new EmbedBuilder()
+.setColor("007fff")
+.setTitle('Loop System')
+.setDescription(`**${queue.current.title}** is now looping.`)
+.setTimestamp()
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+interaction.reply({ embeds: [embed], components:[button]}).then(async Message => {
 
-            const filter = i =>  i.user.id === interaction.user.id
-            let col = await interaction.channel.createMessageComponentCollector({filter, time: 120000 });
-    
-            col.on('collect', async(button) => {
-            if(button.user.id !== interaction.user.id) return
-            
-              switch (button.customId) {
-                case 'loop':
-                    if (queue.repeatMode === 1) return interaction.reply({ content: `You should disable loop mode of existing music first **(/loop)** ❌`, ephemeral: true }).catch(e => { })
-                    const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
-                     interaction.editReply({ content: success ? `Loop Mode: **${queue.repeatMode === 0 ? 'Inactive' : 'Active'}**, The whole sequence will repeat non-stop 🔁` : `Something went wrong. ❌`}).catch(e => { })
-                    await button.deferUpdate();
-                break
-            }
-            })
-            col.on('end', async(button) => {
-                await client.db.delete("loop."+interaction.user.id+interaction.guild.id+interaction.channel.id)
-                 button = new MessageActionRow().addComponents(
-                    new MessageButton()
-                    .setStyle("SUCCESS")
-                    .setLabel("Loop It")
-                    .setCustomId("loop")
-                    .setDisabled(true))
+const filter = i =>  i.user.id === interaction.user.id
+let col = await interaction.channel.createMessageComponentCollector({filter, time: 120000 });
 
-                    const embed = new MessageEmbed()
-                    .setColor("BLUE")
-                    .setTitle('Loop System - Ended')
-                    .setDescription(`Your time is up to choose.`)
-                    .setTimestamp()
-                    .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-                      
-                await interaction.editReply({embeds: [embed], components:[button]}).catch(e => { });
-            })
-        }).catch(e => { })
+col.on('collect', async(button) => {
+if(button.user.id !== interaction.user.id) return
+
+switch (button.customId) {
+case 'loop':
+if (queue.repeatMode === 1) return interaction.reply({ content: `You should disable loop mode of existing music first **(/loop)** ❌`, ephemeral: true }).catch(e => { })
+const success = queue.setRepeatMode(queue.repeatMode === 0 ? QueueRepeatMode.QUEUE : QueueRepeatMode.OFF);
+interaction.editReply({ content: success ? `Loop Mode: **${queue.repeatMode === 0 ? 'Inactive' : 'Active'}**, The whole sequence will repeat non-stop 🔁` : `Something went wrong. ❌`}).catch(e => { })
+await button.deferUpdate();
+break
+}
+})
+col.on('end', async(button) => {
+await db.delete("loop."+interaction.user.id+interaction.guild.id+interaction.channel.id)
+button = new ActionRowBuilder().addComponents(
+new ButtonBuilder()
+.setStyle(ButtonStyle.Success)
+.setLabel("Loop It")
+.setCustomId("loop")
+.setDisabled(true))
+
+const embed = new EmbedBuilder()
+.setColor("007fff")
+.setTitle('Loop System - Ended')
+.setDescription(`Your time is up to choose.`)
+.setTimestamp()
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+
+await interaction.editReply({embeds: [embed], components:[button]}).catch(e => { });
+})
+}).catch(e => { })
 }
 }
