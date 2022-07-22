717664572799124466b448f381d125fe8fffd058
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:31:16 +0300

    v14 updates

--- commands/dj.js
+++ commands/dj.js
@@ -1,63 +1,49 @@
-const { MessageEmbed } = require('discord.js');
-
+const { ApplicationCommandOptionType } = require('discord.js');
+const db = require('croxydb');
 module.exports = {
-    description: "Allows you to set or reset the DJ role.",
-    name: 'dj',
-    options: [
-    {
-        name: "set",
-        description: "Allows you to select a DJ role.",
-        type: 1, 
-        options: [
-            {
-                name: 'role',
-                description: 'Mention a DJ role.',
-                type: 8,
-                required: true
-            }
-        ]
-    },
-    {
-        name: "reset",
-        description: "Allows you to turn off the DJ role.",
-        type: 1,
-        options: []
-    }
+name: "dj",
+description: "Allows you to set or reset the DJ role.",
+permissions: "MANAGE_GUILD",
+options: [{
+name: "set",
+description: "Allows you to select a DJ role.",
+type: ApplicationCommandOptionType.Subcommand, 
+options: [
+{
+name: 'role',
+description: 'Mention a DJ role.',
+type: ApplicationCommandOptionType.Role,
+required: true
+}
+]},
+{
+name: "reset",
+description: "Allows you to turn off the DJ role.",
+type: ApplicationCommandOptionType.Subcommand,
+options: []
+}
 ],
-
 run: async (client, interaction) => {
 
-const db = client.mdb
-if(!interaction.member.permissions.has("MANAGE_GUILD")) return interaction.reply({ content: "You must have **MANAGE GUILD** permission to use this command.", ephemeral: true }).catch(e => { });
 let stp = interaction.options.getSubcommand()
-
 if(stp === "set"){
 const role = interaction.options.getRole('role')
 if(!role) return interaction.reply("If you don't specify a DJ role, you won't be able to use the command!").catch(e => { });
 
-const data = db.findOne({ guildID: interaction.guild.id }).catch(e => { });
+await db.set(`dj-${interaction.guild.id}`, role.id)
+return await interaction.reply({ content: "The DJ role is successfully set to <@&"+role+">.", ephemeral: true }).catch(e => { });
+
+} 
+if(stp === "reset"){
+const data = db.get(`dj-${interaction.guild.id}`)
+
 if(data){
-await db.updateOne({ guildID: interaction.guild.id}, {
-        $set: {
-            djRole: role.id    
-        }}, { upsert: true }).catch(e => { });
-        return await interaction.reply({ content: "The DJ role is successfully set to <@&"+role+">.", ephemeral: true }).catch(e => { });
-    } else {
-        await db.findOneAndUpdate({ guildID: interaction.guild.id}, {
-            $set: {
-                djRole: role.id
-            }}, { upsert: true }).catch(e => { });
-            return awaitinteraction.reply({ content: "The DJ role is successfully set to <@&"+role+">.", ephemeral: true }).catch(e => { });
-        }
-    } 
-    if(stp === "reset"){
-        const data = db.findOne({ guildID: interaction.guild.id }).catch(e => { });
-if(data){
-await db.deleteOne({ guildID: interaction.guild.id }).catch(e => { })
+await db.delete(`dj-${interaction.guild.id}`)
 return await interaction.reply({ content: "The DJ role is successfully deleted.", ephemeral: true }).catch(e => { });
 } else {
 return await interaction.reply({ content: "The DJ role is not already set.", ephemeral: true }).catch(e => { });
 }
-    }
-    },
+
+}
+},
 };
