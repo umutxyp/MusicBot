38f81811f366a4b8290eaeb43e73d8948798a680
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:30:54 +0300

    v14 updates

--- commands/clear.js
+++ commands/clear.js
@@ -1,18 +1,17 @@
 module.exports = {
-    description: "Clears the music queue.",
-    name: 'clear',
-    options: [],
-    voiceChannel: true,
+name: "clear",
+description: "Clears the music queue.",
+permissions: "SEND_MESSAGES",
+options: [],
+run: async (client, interaction) => {
+const queue = client.player.getQueue(interaction.guild.id);
 
-    run: async (client, interaction) => {
-        const queue = client.player.getQueue(interaction.guild.id);
+if (!queue || !queue.playing) return interaction.reply({ content: `No music currently playing. ❌`, ephemeral: true }).catch(e => { })
 
-        if (!queue || !queue.playing) return interaction.reply({ content: `No music currently playing. ❌`, ephemeral: true }).catch(e => { })
+if (!queue.tracks[0]) return interaction.reply({ content: `There is already no music in queue after the current one ❌`, ephemeral: true }).catch(e => { })
 
-        if (!queue.tracks[0]) return interaction.reply({ content: `There is already no music in queue after the current one ❌`, ephemeral: true }).catch(e => { })
+await queue.clear();
 
-        await queue.clear();
-
-        interaction.reply({ content: `The queue has just been cleared. 🗑️` }).catch(e => { })
-    },
-};
+interaction.reply({ content: `The queue has just been cleared. 🗑️` }).catch(e => { })
+},
+}
