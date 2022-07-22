c8ee8899ccb860d59de37dea1bd1f0b783b4364f
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 02:32:41 +0300

    Update save.js

--- commands/save.js
+++ commands/save.js
@@ -1,30 +1,31 @@
-const Discord = require('discord.js');
+const { EmbedBuilder } = require('discord.js');
 module.exports = {
-    description: "It sends and saves the played music to you via dm box.",
-    name: 'save',
-    options: [],
-    voiceChannel: true,
+name: "save",
+description: "It sends and saves the played music to you via dm box.",
+permissions: "SEND_MESSAGES",
+options: [],
+run: async (client, interaction) => {
+const queue = client.player.getQueue(interaction.guild.id);
 
-    run: async (client, interaction) => {
-        const queue = client.player.getQueue(interaction.guild.id);
+if (!queue || !queue.playing) return interaction.reply({ content: `There is no music currently playing!. ❌`, ephemeral: true }).catch(e => { })
 
-  if (!queue || !queue.playing) return interaction.reply({ content: `There is no music currently playing!. ❌`, ephemeral: true }).catch(e => { })
-
-  const embed = new Discord.MessageEmbed()
-  .setColor('BLUE')
-  .setTitle(client.user.username + " - Save Track")
-  .setThumbnail(client.user.displayAvatarURL())
-  .addField(`Track`, `\`${queue.current.title}\``)
-  .addField(`Duration`, `\`${queue.current.duration}\``)
-  .addField(`URL`, `${queue.current.url}`)
-  .addField(`Saved Server`, `\`${interaction.guild.name}\``)
-  .addField(`Requested By`, `${queue.current.requestedBy}`)
-  .setTimestamp()
-  .setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
-  interaction.user.send({ embeds: [embed] }).then(() => {
-    interaction.reply({ content: `I sent the name of the music via private message. ✅`, ephemeral: true }).catch(e => { })
-        }).catch(error => {
-            interaction.reply({ content: `Unable to send you private message. ❌`, ephemeral: true }).catch(e => { })
-        });
-    },
+const embed = new EmbedBuilder()
+.setColor('007fff')
+.setTitle(client.user.username + " - Save Track")
+.setThumbnail(client.user.displayAvatarURL())
+.addFields([
+{ name: `Track`, value: `\`${queue.current.title}\`` },
+{ name: `Duration`, value: `\`${queue.current.duration}\`` },
+{ name: `URL`, value: `${queue.current.url}` },
+{ name: `Saved Server`, value: `\`${interaction.guild.name}\`` },
+{ name: `Requested By`, value: `${queue.current.requestedBy}` }
+])
+.setTimestamp()
+.setFooter({text: `Code Share - by Umut Bayraktar ❤️` })
+interaction.user.send({ embeds: [embed] }).then(() => {
+interaction.reply({ content: `I sent the name of the music via private message. ✅`, ephemeral: true }).catch(e => { })
+}).catch(error => {
+interaction.reply({ content: `Unable to send you private message. ❌`, ephemeral: true }).catch(e => { })
+});
+},
 };
