113ba138e2fd4f8162dcde5472989fe3fc5e423b
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-07-23 17:09:04 +0300

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,7 +4,7 @@ const { ApplicationCommandOptionType } = require('discord.js');
 module.exports = {
 name: "play",
 description: "It helps you start a new music.",
-permissions: "SEND_MESSAGES",
+permissions: "0x0000000000000800",
 options: [{
 name: 'musics',
 description: 'Type the name of the music you want to play.',
