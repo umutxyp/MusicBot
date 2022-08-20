0ce62279d538d61440bcdc8ebffc57dcd79ce8b3
xWass <73602180+xWass@users.noreply.github.com>
2022-08-20 13:43:46 -0400

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: "back",
-    description: "Plays the previous music again.",
+    description: "Plays the previous track.",
     permissions: "0x0000000000000800",
     options: [],
     run: async (client, interaction) => {
@@ -9,10 +9,10 @@ module.exports = {
 
         if (!queue || !queue.playing) return interaction.reply({ content: `No music currently playing! ❌`, ephemeral: true }).catch(e => { })
 
-        if (!queue.previousTracks[1]) return interaction.reply({ content: `There was no music playing before ❌`, ephemeral: true }).catch(e => { })
+        if (!queue.previousTracks[1]) return interaction.reply({ content: `There is no previous track! ❌`, ephemeral: true }).catch(e => { })
 
         await queue.back();
 
-        interaction.reply({ content: `Previous music started playing... ✅` }).catch(e => { })
+        interaction.reply({ content: `Now playing ${queue.previousTracks[1].title}. ✅` }).catch(e => { })
     },
 };
