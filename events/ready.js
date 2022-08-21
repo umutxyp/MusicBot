9d0975d1721f5476bd146711c30a6454b87ac4fd
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-21 12:31:34 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -16,7 +16,26 @@ module.exports = async (client) => {
     })();
 
     console.log(client.user.username + " successfully connected.");
-    client.user.setStatus('ONLINE');
-    client.user.setActivity(config.status)
+    console.log(`EN | ERROR EXAMPLE:IF THE BOT IS WORKING, RESPONDING TO COMMANDS BUT NO SOUND, FOLLOW THESE STEPS;
+Enter the page of your project that you opened on replit.com.
+Click on the one that says shell from the boxes that say console and shell above the opened box at the bottom right.
+Type npm uni discord-player
+Type npm i discord-player@5.3.0-dev.2
+Type npm uni ytdl-core
+Type npm i ytdl-core@4.10.0
+Turn off and on the bot from the button that says "Stop" or "Run" in the middle of the replit screen
+
+
+TR | HATA ÖRNEĞİ: BOT ÇALIŞIYOR, KOMUTLARA YANIT VERİYOR ANCAK SES YOKSA BU ADIMLARI TAKİP EDİN;
+replit.com'da açtığınız projenizin sayfasına giriniz.
+Sağ altta açılan kutunun üzerinde konsol ve kabuk yazan kutulardan kabuk yazana tıklayın.
+npm uni discord-player yazın
+npm i discord-player@5.3.0-dev.2 yazın
+npm uni ytdl-core yazın
+npm i ytdl-core@4.10.0 yazın
+Ekranın ortasındaki "Srop" veya "Run" yazan butondan botu kapatıp açın.`)
+    
+client.user.setStatus('ONLINE');
+client.user.setActivity(config.status)
 
 }
