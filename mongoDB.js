99e7d3573801b19127e4b82f158b3655e1eaae3d
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-27 21:21:55 +0300

    Create mongoDB.js

---
+++ mongoDB.js
@@ -0,0 +1,50 @@
+const { Schema, model } = require("mongoose");
+
+const musicbot = Schema({
+guildID: String,
+role: String,
+volume: Number
+})
+
+const loop = Schema({
+userID: String,
+guildID: String,
+channelID: String,
+messageID: String
+})
+
+const queue = Schema({
+userID: String,
+guildID: String,
+channelID: String,
+messageID: String
+})
+
+const playlist = Schema({
+userID: String,
+playlist: Array,
+musics: Array
+})
+
+const playlist_timer = Schema({
+userID: String,
+guildID: String,
+channelID: String,
+messageID: String
+})
+
+const playlist_timer2 = Schema({
+userID: String,
+guildID: String,
+channelID: String,
+messageID: String
+})
+
+module.exports = {
+musicbot: model("musicbot", musicbot),
+loop: model("loop", loop),
+queue: model("queue", queue),
+playlist: model("playlist", playlist),
+playlist_timer: model("playlist_timer", playlist_timer),
+playlist_timer2: model("playlist_timer2", playlist_timer2)
+}
