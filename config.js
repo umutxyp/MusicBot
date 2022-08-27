2e3dad532bfd985df8b76734aa5c5fd39b53eb13
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-27 21:21:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,40 @@
 module.exports = {
-    TOKEN: "",
-    ownerID: "", //write your discord user id.
-    botInvite: "", //write your discord bot invite.
-    status: '❤️ Umut Bayraktar',
-    commandsDir: './commands', //Please don't touch
+TOKEN: "",
+ownerID: "", //write your discord user id.
+botInvite: "", //write your discord bot invite.
+mongodbURL: "", //write your mongodb url.
+status: '❤️ codeshare.me',
+commandsDir: './commands', //Please don't touch
+language: "en", //en, tr
 
-    opt: {
-        DJ: {
-            commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
-        },
+opt: {
+DJ: {
+commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
+},
 
-        voiceConfig: {
-            leaveOnEnd: false, //If this variable is "true", the bot will leave the channel the music ends.
-            autoSelfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
+voiceConfig: {
+leaveOnEnd: false, //If this variable is "true", the bot will leave the channel the music ends.
+autoSelfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
 
-            leaveOnTimer: { //The leaveOnEnd variable must be "false" to use this system.
-                status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
-                time: 20000, //1000 = 1 second
-            }
-        },
+leaveOnEmpty: { //The leaveOnEnd variable must be "false" to use this system.
+status: false, //If this variable is "true", the bot will leave the channel when the bot is offline.
+cooldown: 20000, //1000 = 1 second
+},
 
-        maxVol: 100, //You can specify the maximum volume level.
-        loopMessage: false,
+leaveOnTimer: { //The leaveOnEnd variable must be "false" to use this system.
+status: false, //If this variable is "true", the bot will leave the channel when the bot is offline.
+cooldown: 20000, //1000 = 1 second
+}
+},
 
-        discordPlayer: {
-            ytdlOptions: {
-                quality: 'highestaudio', //Please don't touch
-                highWaterMark: 1 << 25 //Please don't touch
-            }
-        }
-    }
+maxVol: 252, //You can specify the maximum volume level.
+loopMessage: true,
+
+discordPlayer: {
+ytdlOptions: {
+quality: 'highestaudio', //Please don't touch
+highWaterMark: 1 << 25 //Please don't touch
+}
+}
+}
 }
