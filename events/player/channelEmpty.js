f81e5507bef25f05eb252548d1d332b16b0f1e3f
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-28 20:11:46 +0300

    Update channelEmpty.js

--- events/player/channelEmpty.js
+++ events/player/channelEmpty.js
@@ -1,7 +1,9 @@
 module.exports = async (client, queue) => {
     if (queue) {
       if (queue?.metadata) {
+        if(client?.config?.opt?.voiceConfig?.leaveOnEmpty?.status === true){
         queue?.metadata?.send({ content: `${client.language.msg15}` }).catch(e => { })
+        }
       }
     }
   }
