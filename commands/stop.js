3c78a379c29630772bb8ed05098c5030af13364e
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-29 23:33:58 +0300

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -3,8 +3,9 @@ name: "stop",
 description: "Plays the previous music again.",
 permissions: "0x0000000000000800",
 options: [],
+voiceChannel: true,
 run: async (client, interaction) => {
-    let lang = client.language
+let lang = client.language
 const queue = client.player.getQueue(interaction.guild.id);
 
 if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
