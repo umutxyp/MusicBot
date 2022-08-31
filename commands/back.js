9e8e020425b26d26c5d604d97868516190dffb16
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:29:12 +0300

    Update back.js

--- commands/back.js
+++ commands/back.js
@@ -1,3 +1,4 @@
+const db = require("../mongoDB");
 module.exports = {
 name: "back",
 description: "Plays the previous track.",
@@ -5,7 +6,9 @@ permissions: "0x0000000000000800",
 options: [],
   voiceChannel: true,
 run: async (client, interaction) => {
-let lang = client.language
+let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
+lang = lang?.language || client.language
+lang = require(`../languages/${lang}.js`);
 const queue = client.player.getQueue(interaction.guild.id);
 
 if (!queue || !queue.playing) return interaction.reply({ content: `${lang.msg5}`, ephemeral: true }).catch(e => { })
