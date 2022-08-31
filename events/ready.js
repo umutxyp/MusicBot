15408ed3eafd266881295561060ad3d72c8efd42
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:34:47 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,46 +1,51 @@
 const config = require("../config.js");
+const fs = require('fs');
 module.exports = async (client) => {
-  let lang = client.language
-  const { REST } = require("@discordjs/rest");
-  const { Routes } = require("discord-api-types/v10");
-  const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-  (async () => {
-    try {
-      await rest.put(Routes.applicationCommands(client.user.id), {
-        body: await client.commands,
-      });
-      console.log(lang.loadslash)
-    } catch (err) {
-      console.log(lang.error3 + err);
-    }
-  })();
+let lang = client.language
+lang = require(`../languages/${lang}.js`);
 
-  if (config.mongodbURL || process.env.MONGO) {
-    const mongoose = require("mongoose")
-    mongoose.connect(config.mongodbURL || process.env.MONGO, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then(async () => {
-      console.log(`Connected MongoDB`)
+if (config.mongodbURL || process.env.MONGO) {
+const mongoose = require("mongoose")
+mongoose.connect(config.mongodbURL || process.env.MONGO, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+}).then(async () => {
+console.log(`Connected MongoDB`)
 
-      setTimeout(async () => {
-        const db = require("../mongoDB");
-        await db.loop.deleteOne()
-        await db.queue.deleteOne()
-        await db.playlist_timer.deleteOne()
-        await db.playlist_timer2.deleteOne()
-      }, 5000)
-    }).catch((err) => {
-      console.log("MongoDB Error: " + err)
-    })
-  } else {
-    console.log(lang.error4)
-  }
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
+(async () => {
+try {
+await rest.put(Routes.applicationCommands(client.user.id), {
+body: await client.commands,
+});
+console.log(lang.loadslash)
+} catch (err) {
+console.log(lang.error3 + err);
+}
+})();
 
-  console.log(client.user.username + lang.ready);
-  console.log(`${lang.error5}`)
+console.log(client.user.username + lang.ready);
+console.log(`${lang.error5}`)
+  
+client.user.setStatus('ONLINE');
+client.user.setActivity(config.status)
 
-  client.user.setStatus('ONLINE');
-  client.user.setActivity(config.status)
+setTimeout(async () => {
+const db = require("../mongoDB");
+await db.loop.deleteOne()
+await db.queue.deleteOne()
+await db.playlist_timer.deleteOne()
+await db.playlist_timer2.deleteOne()
+}, 5000)
+
+}).catch((err) => {
+console.log("\nMongoDB Error: " + err + "\n\n" + lang.error4)
+})
+
+} else {
+console.log(lang.error4)
+}
 
 }
