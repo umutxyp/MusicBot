72f215c541293f8e03a5bf341c0ee120a41dbef2
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:33:54 +0300

    Update connectionError.js

--- events/player/connectionError.js
+++ events/player/connectionError.js
@@ -1,8 +1,8 @@
 module.exports = async (client, queue, error) => {
-    if (queue) {
-      queue?.destroy();
-      if (queue?.metadata) {
-        queue?.metadata?.send({ content: `${error} ❌ ` }).catch(e => { })
-      }
-    }
-  }
+if (queue) {
+queue?.destroy();
+if (queue?.metadata) {
+queue?.metadata?.send({ content: `${error} ❌ ` }).catch(e => { })
+}
+}
+}
