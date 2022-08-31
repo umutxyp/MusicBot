f0b37c3f7734c6ea9e18fca81910efa37e28eb50
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:34:20 +0300

    Update trackStart.js

--- events/player/trackStart.js
+++ events/player/trackStart.js
@@ -1,8 +1,12 @@
+const db = require("../../mongoDB");
 module.exports = async (client, queue, track) => {
-    if (queue) {
-      if (!client.config.opt.loopMessage && queue?.repeatMode !== 0) return;
-      if (queue?.metadata) {
-        queue?.metadata?.send({ content: client.language.msg13.replace("{track?.title}", track?.title).replace("{queue?.connection.channel.name}", queue?.connection.channel.name)}).catch(e => { });
-      }
-    }
-  }
+let lang = await db?.musicbot?.findOne({ guildID: queue?.metadata?.guild?.id })
+lang = lang?.language || client.language
+lang = require(`../../languages/${lang}.js`);
+if (queue) {
+if (!client.config.opt.loopMessage && queue?.repeatMode !== 0) return;
+if (queue?.metadata) {
+queue?.metadata?.send({ content: lang.msg13.replace("{track?.title}", track?.title).replace("{queue?.connection.channel.name}", queue?.connection.channel.name)}).catch(e => { });
+}
+}
+}
