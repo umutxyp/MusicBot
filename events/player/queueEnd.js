03372edfe22968b5f329640c067bd7705e136735
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:34:10 +0300

    Update queueEnd.js

--- events/player/queueEnd.js
+++ events/player/queueEnd.js
@@ -1,16 +1,20 @@
+const db = require("../../mongoDB");
 module.exports = async (client, queue) => {
-    if (client.config.opt.voiceConfig.leaveOnTimer.status === true) {
-      if (queue) {
-        setTimeout(async() => {
-          if (queue?.connection) {
-            if (!queue?.playing) { //additional check in case something new was added before time was up
-              await queue?.connection?.disconnect()
-            }
-          };
-        }, client.config.opt.voiceConfig.leaveOnTimer.cooldown);
-      }
-    }
-      if (queue?.metadata) {
-        queue?.metadata?.send({ content: `${client.language.msg14}` }).catch(e => { })
-      }
-    }
+let lang = await db?.musicbot?.findOne({ guildID: queue?.metadata?.guild?.id })
+lang = lang?.language || client.language
+lang = require(`../../languages/${lang}.js`);
+if (client.config.opt.voiceConfig.leaveOnTimer.status === true) {
+if (queue) {
+setTimeout(async() => {
+if (queue?.connection) {
+if (!queue?.playing) { //additional check in case something new was added before time was up
+await queue?.connection?.disconnect()
+}
+};
+}, client.config.opt.voiceConfig.leaveOnTimer.cooldown);
+}
+}
+if (queue?.metadata) {
+queue?.metadata?.send({ content: `${lang.msg14}` }).catch(e => { })
+}
+}
