2c6ba66090408f9ef7c38f962e529eab5256be15
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:34:03 +0300

    Update error.js

--- events/player/error.js
+++ events/player/error.js
@@ -1,7 +1,7 @@
 module.exports = async (client, queue, error) => {
-    if (queue) {
-      if (queue?.metadata) {
-        queue?.metadata?.send({ content: `${error} ❌ ` }).catch(e => { })
-      }
-    }
-  }
+if (queue) {
+if (queue?.metadata) {
+queue?.metadata?.send({ content: `${error} ❌ ` }).catch(e => { })
+}
+}
+}
