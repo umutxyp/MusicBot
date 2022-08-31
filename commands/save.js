a47ce10abd7a74ed4434eb1349ef6eb63468579f
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:32:19 +0300

    Update save.js

--- commands/save.js
+++ commands/save.js
@@ -1,11 +1,14 @@
 const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+const db = require("../mongoDB");
 module.exports = {
 name: "save",
 description: "It sends and saves the played music to you via dm box.",
 permissions: "0x0000000000000800",
 options: [],
 run: async (client, interaction) => {
-    let lang = client.language
+let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
+lang = lang?.language || client.language
+lang = require(`../languages/${lang}.js`);
 const queue = client.player.getQueue(interaction.guild.id);
 
 if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
