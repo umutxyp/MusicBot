f5f81bc6cf75bf826fd719a4c7cc60e8c056e000
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 12:27:56 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -4,7 +4,9 @@ const { EmbedBuilder, InteractionType, ModalBuilder, TextInputBuilder, TextInput
 const db = require("../mongoDB");
 
 module.exports = async (client, interaction) => {
-
+if (!interaction.guild){
+return interaction.reply({ content: "This bot is only for servers and can be used on servers.", ephemeral: true })
+} else {
 let lang = await db?.musicbot?.findOne({ guildID: interaction?.guild?.id })
 lang = lang?.language || client.language
 lang = require(`../languages/${lang}.js`);
@@ -154,5 +156,5 @@ return interaction.reply(`<@${interaction.user.id}>, **${queue.current.title}** 
 break
 }
 }
-
+}
 }
