a58324a2ad4443cd55439f639ec91ff47ab0fed8
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:29:47 +0300

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,3 +1,4 @@
+const db = require("../mongoDB");
 module.exports = {
 name: "clear",
 description: "Clears the music queue.",
@@ -6,7 +7,9 @@ options: [],
   voiceChannel: true,
 run: async (client, interaction) => {
 const queue = client.player.getQueue(interaction.guild.id);
-let lang = client.language
+let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
+lang = lang?.language || client.language
+lang = require(`../languages/${lang}.js`);
 if (!queue || !queue.playing) return interaction.reply({ content: `${lang.msg5}`, ephemeral: true }).catch(e => { })
 
 if (!queue.tracks[0]) return interaction.reply({ content: `${lang.msg23}`, ephemeral: true }).catch(e => { })
