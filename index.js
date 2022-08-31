e91c03815b945b66761f35e5a35cdaf29f389260
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-08-31 04:37:16 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,37 @@ const { Player } = require('discord-player');
 const config = require("./config")
 const fs = require('fs');
 const client = new Client({
-  intents: [
-    GatewayIntentBits.Guilds, // for guild related things
-    GatewayIntentBits.GuildMembers, // for guild members related things
-    GatewayIntentBits.GuildIntegrations, // for discord Integrations
-    GatewayIntentBits.GuildVoiceStates, // for voice related things
-    GatewayIntentBits.GuildMessages, // for guild messages things
-    GatewayIntentBits.GuildMessageTyping, // for message typing things
-    GatewayIntentBits.MessageContent // enable if you need message content things
-  ],
+partials: [
+Partials.Channel, // for text channel
+Partials.GuildMember, // for guild member
+Partials.User, // for discord user
+],
+intents: [
+GatewayIntentBits.Guilds, // for guild related things
+GatewayIntentBits.GuildMembers, // for guild members related things
+GatewayIntentBits.GuildIntegrations, // for discord Integrations
+GatewayIntentBits.GuildVoiceStates, // for voice related things
+]
 })
 
 client.config = config;
 client.player = new Player(client, client.config.opt.discordPlayer);
 const player = client.player
 player.setMaxListeners(200);
-client.language = require(`./languages/${config.language || "en"}.js`)
-let lang = client.language
+client.language = config.language || "en"
+let lang = require(`./languages/${config.language || "en"}.js`)
 
 fs.readdir("./events", (_err, files) => {
-  files.forEach((file) => {
-    if (!file.endsWith(".js")) return;
-    const event = require(`./events/${file}`);
-    let eventName = file.split(".")[0];
-    console.log(`${lang.loadclientevent}: ${eventName}`);
-    client.on(eventName, event.bind(null, client));
-    delete require.cache[require.resolve(`./events/${file}`)];
-  });
+files.forEach((file) => {
+if (!file.endsWith(".js")) return;
+const event = require(`./events/${file}`);
+let eventName = file.split(".")[0];
+console.log(`${lang.loadclientevent}: ${eventName}`);
+client.on(eventName, event.bind(null, client));
+delete require.cache[require.resolve(`./events/${file}`)];
 });
+});
+
 
 fs.readdir("./events/player", (_err, files) => {
 files.forEach((file) => {
@@ -43,38 +46,39 @@ delete require.cache[require.resolve(`./events/player/${file}`)]
 })
 })
 
+
 client.commands = [];
 fs.readdir(config.commandsDir, (err, files) => {
-  if (err) throw err;
-  files.forEach(async (f) => {
-    try {
-      let props = require(`${config.commandsDir}/${f}`);
-      client.commands.push({
-        name: props.name,
-        description: props.description,
-        options: props.options
-      });
-      console.log(`${lang.loadcmd}: ${props.name}`);
-    } catch (err) {
-      console.log(err);
-    }
-  });
+if (err) throw err;
+files.forEach(async (f) => {
+try {
+let props = require(`${config.commandsDir}/${f}`);
+client.commands.push({
+name: props.name,
+description: props.description,
+options: props.options
+});
+console.log(`${lang.loadcmd}: ${props.name}`);
+} catch (err) {
+console.log(err);
+}
+});
 });
 
 
 if (config.TOKEN || process.env.TOKEN) {
-  client.login(config.TOKEN || process.env.TOKEN).catch(e => {
-    console.log(lang.error1)
-  })
+client.login(config.TOKEN || process.env.TOKEN).catch(e => {
+console.log(lang.error1)
+})
 } else {
-  setTimeout(() => {
-    console.log(lang.error2)
-  }, 2000)
+setTimeout(() => {
+console.log(lang.error2)
+}, 2000)
 }
 
 const express = require("express");
 const app = express();
 app.get("/", (request, response) => {
-  response.sendStatus(200);
+response.sendStatus(200);
 });
 app.listen(process.env.PORT);
