26e4777c5ffcc68319fb85d537eef33d74f9d4e3
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-01 19:48:13 +0300

    Update time.js

--- commands/time.js
+++ commands/time.js
@@ -9,28 +9,59 @@ run: async (client, interaction) => {
 let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
 lang = lang?.language || client.language
 lang = require(`../languages/${lang}.js`);
-const queue = client.player.getQueue(interaction.guild.id);
 
-if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+try {
 
-const progress = queue.createProgressBar();
-const timestamp = queue.getPlayerTimestamp();
+    const queue = client.player.getQueue(interaction.guild.id);
 
-if (timestamp.progress == 'Infinity') return interaction.reply({ content: lang.msg84, ephemeral: true }).catch(e => { })
+    if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+    
+    const progress = queue.createProgressBar();
+    const timestamp = queue.getPlayerTimestamp();
+    
+    if (timestamp.progress == 'Infinity') return interaction.reply({ content: lang.msg84, ephemeral: true }).catch(e => { })
+    
+    const saveButton = new ButtonBuilder();
+    saveButton.setLabel(lang.msg86);
+    saveButton.setCustomId('time');
+    saveButton.setStyle(ButtonStyle.Success);
+    const row = new ActionRowBuilder().addComponents(saveButton);
+    
+    const embed = new EmbedBuilder()
+    .setColor(client.config.embedColor)
+    .setTitle(queue.current.title)
+    .setThumbnail(client.user.displayAvatarURL())
+    .setTimestamp()
+    .setDescription(`${progress} (**${timestamp.progress}**%)`)
+    .setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
+    interaction.reply({ embeds: [embed], components: [row] }).catch(e => { })
 
-const saveButton = new ButtonBuilder();
-saveButton.setLabel(lang.msg86);
-saveButton.setCustomId('time');
-saveButton.setStyle(ButtonStyle.Success);
-const row = new ActionRowBuilder().addComponents(saveButton);
-
-const embed = new EmbedBuilder()
-.setColor(client.config.embedColor)
-.setTitle(queue.current.title)
-.setThumbnail(client.user.displayAvatarURL())
+} catch (e) {
+    if(client.errorLog){
+let embed = new EmbedBuilder()
+.setColor(config.embedColor)
 .setTimestamp()
-.setDescription(`${progress} (**${timestamp.progress}**%)`)
-.setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
-interaction.reply({ embeds: [embed], components: [row] }).catch(e => { })
+.addFields([
+        { name: "Command", value: `${interaction?.commandName}` },
+        { name: "Error", value: `${e.stack}` },
+        { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
+        { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
+        { name: "Time", value: `<t:${Math.floor(Date.now()/1000)}:R>`, inline: true },
+        { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
+        { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
+    ])
+    await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+    } else {
+    console.log(`
+    Command: ${interaction?.commandName}
+    Error: ${e}
+    User: ${interaction?.user?.tag} (${interaction?.user?.id})
+    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
+    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
+    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
+    `)
+    }
+    return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+    }
 },
 };
