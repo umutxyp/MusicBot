fa9e2e0b19abf5bfdf3f4d918b96e33f10df072e
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-01 19:45:19 +0300

    Update language.js

--- commands/language.js
+++ commands/language.js
@@ -10,71 +10,100 @@ run: async (client, interaction) => {
 let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
 lang = lang?.language || client.language
 lang = require(`../languages/${lang}.js`);
+try {
 
-let buttons = new ActionRowBuilder().addComponents(
-new ButtonBuilder()
-.setLabel("Türkçe")
-.setCustomId('tr')
-.setStyle(ButtonStyle.Success),
-new ButtonBuilder()
-.setLabel("English")
-.setCustomId('en')
-.setStyle(ButtonStyle.Success))
+    let buttons = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setLabel("Türkçe")
+        .setCustomId('tr')
+        .setStyle(ButtonStyle.Success),
+        new ButtonBuilder()
+        .setLabel("English")
+        .setCustomId('en')
+        .setStyle(ButtonStyle.Success))
+        
+        let embed = new EmbedBuilder()
+        .setColor(client.config.embedColor)
+        .setTitle("Select a language")
+        .setTimestamp()
+        .setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
+        interaction.reply({ embeds: [embed], components: [buttons] }).then(async Message => {
+        
+        const filter = i => i.user.id === interaction.user.id
+        let col = await interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
+        
+        col.on('collect', async (button) => {
+        if (button.user.id !== interaction.user.id) return
+        switch (button.customId) {
+        case 'tr':
+        await db?.musicbot?.updateOne({ guildID: interaction.guild.id }, { 
+        $set: { 
+        language: 'tr' 
+        } 
+        }, { upsert: true }).catch(e => { })
+        await interaction.editReply({ content: `Botun dili başarıyla türkçe oldu.`, embeds:[], components:[], ephemeral: true }).catch(e => { })
+        await button.deferUpdate().catch(e => {})
+        await col.stop()
+        break
+        case 'en':
+        await db?.musicbot?.updateOne({ guildID: interaction.guild.id }, { 
+        $set: { 
+        language: 'en' 
+        } 
+        }, { upsert: true }).catch(e => { })
+        await interaction.editReply({ content: `Bot language successfully changed to english.`, embeds:[], components:[], ephemeral: true }).catch(e => { })
+        await button.deferUpdate().catch(e => {})
+        await col.stop()
+        break
+        }
+        })
+        
+        col.on('end', async (button, reason) => {
+        if (reason === 'time') {
+        buttons = new ActionRowBuilder().addComponents(
+        new ButtonBuilder()
+        .setStyle(ButtonStyle.Secondary)
+        .setLabel(lang.msg45)
+        .setCustomId("timeend")
+        .setDisabled(true))
+        
+        embed = new EmbedBuilder()
+        .setColor(client.config.embedColor)
+        .setTitle("Time ended, please try again.")
+        .setTimestamp()
+        .setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
+        
+        await interaction.editReply({ embeds: [embed], components: [buttons] }).catch(e => {})
+        }
+        })
+        }).catch(e => { })
 
-let embed = new EmbedBuilder()
-.setColor(client.config.embedColor)
-.setTitle("Select a language")
-.setTimestamp()
-.setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
-interaction.reply({ embeds: [embed], components: [buttons] }).then(async Message => {
-
-const filter = i => i.user.id === interaction.user.id
-let col = await interaction.channel.createMessageComponentCollector({ filter, time: 30000 });
-
-col.on('collect', async (button) => {
-if (button.user.id !== interaction.user.id) return
-switch (button.customId) {
-case 'tr':
-await db?.musicbot?.updateOne({ guildID: interaction.guild.id }, { 
-$set: { 
-language: 'tr' 
-} 
-}, { upsert: true }).catch(e => { })
-await interaction.editReply({ content: `Botun dili başarıyla türkçe oldu.`, embeds:[], components:[], ephemeral: true }).catch(e => { })
-await button.deferUpdate().catch(e => {})
-await col.stop()
-break
-case 'en':
-await db?.musicbot?.updateOne({ guildID: interaction.guild.id }, { 
-$set: { 
-language: 'en' 
-} 
-}, { upsert: true }).catch(e => { })
-await interaction.editReply({ content: `Bot language successfully changed to english.`, embeds:[], components:[], ephemeral: true }).catch(e => { })
-await button.deferUpdate().catch(e => {})
-await col.stop()
-break
-}
-})
-
-col.on('end', async (button, reason) => {
-if (reason === 'time') {
-buttons = new ActionRowBuilder().addComponents(
-new ButtonBuilder()
-.setStyle(ButtonStyle.Secondary)
-.setLabel(lang.msg45)
-.setCustomId("timeend")
-.setDisabled(true))
-
-embed = new EmbedBuilder()
-.setColor(client.config.embedColor)
-.setTitle("Time ended, please try again.")
-.setTimestamp()
-.setFooter({ text: `codeshare.me | Umut Bayraktar ❤️` })
-
-await interaction.editReply({ embeds: [embed], components: [buttons] }).catch(e => {})
-}
-})
-}).catch(e => { })
+    } catch (e) {
+        if(client.errorLog){
+    let embed = new EmbedBuilder()
+    .setColor(config.embedColor)
+    .setTimestamp()
+    .addFields([
+            { name: "Command", value: `${interaction?.commandName}` },
+            { name: "Error", value: `${e.stack}` },
+            { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
+            { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
+            { name: "Time", value: `<t:${Math.floor(Date.now()/1000)}:R>`, inline: true },
+            { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
+            { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
+        ])
+        await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+        } else {
+        console.log(`
+        Command: ${interaction?.commandName}
+        Error: ${e}
+        User: ${interaction?.user?.tag} (${interaction?.user?.id})
+        Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
+        Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
+        User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
+        `)
+        }
+        return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+        }
 },
 }
