96254c1304304d8ac9d50847e90b1f6fe1d767f6
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-01 19:47:11 +0300

    Update resume.js

--- commands/resume.js
+++ commands/resume.js
@@ -10,10 +10,39 @@ const queue = client.player.getQueue(interaction.guild.id);
 let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
 lang = lang?.language || client.language
 lang = require(`../languages/${lang}.js`);
-if (!queue) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+try {
 
-const success = queue.setPaused(false);
+    if (!queue) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+    const success = queue.setPaused(false);
+    return interaction.reply({ content: success ? `**${queue.current.title}**, ${lang.msg72}` : lang.msg71 }).catch(e => { })
 
-return interaction.reply({ content: success ? `**${queue.current.title}**, ${lang.msg72}` : lang.msg71 }).catch(e => { })
+} catch (e) {
+    if(client.errorLog){
+const { EmbedBuilder } = require("discord.js")
+let embed = new EmbedBuilder()
+.setColor(config.embedColor)
+.setTimestamp()
+.addFields([
+        { name: "Command", value: `${interaction?.commandName}` },
+        { name: "Error", value: `${e.stack}` },
+        { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
+        { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
+        { name: "Time", value: `<t:${Math.floor(Date.now()/1000)}:R>`, inline: true },
+        { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
+        { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
+    ])
+    await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+    } else {
+    console.log(`
+    Command: ${interaction?.commandName}
+    Error: ${e}
+    User: ${interaction?.user?.tag} (${interaction?.user?.id})
+    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
+    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
+    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
+    `)
+    }
+    return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+    }
 },
 };
