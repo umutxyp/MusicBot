82ac0e7b32324180872900c371ceef5554c24699
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-01 19:47:50 +0300

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -1,4 +1,4 @@
-const { ApplicationCommandOptionType } = require('discord.js');
+const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
 name: "skip",
@@ -15,17 +15,47 @@ run: async (client, interaction) => {
 let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
 lang = lang?.language || client.language
 lang = require(`../languages/${lang}.js`);
-const queue = client.player.getQueue(interaction.guild.id);
-if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+try {
 
-const number = interaction.options.getNumber('number');
-if (number) {
-if(number > queue.tracks.length) return interaction.reply({ content: lang.msg82, ephemeral: true }).catch(e => { })
-await queue.skipTo(number);
-return interaction.reply({ content: `**${queue.current.title}**, ${lang.msg83}` }).catch(e => { })
-} else {
-const success = queue.skip();
-return interaction.reply({ content: success ? `**${queue.current.title}**, ${lang.msg83}` : lang.msg41 }).catch(e => { })
-}
+    const queue = client.player.getQueue(interaction.guild.id);
+    if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+    
+    const number = interaction.options.getNumber('number');
+    if (number) {
+    if(number > queue.tracks.length) return interaction.reply({ content: lang.msg82, ephemeral: true }).catch(e => { })
+    await queue.skipTo(number);
+    return interaction.reply({ content: `**${queue.current.title}**, ${lang.msg83}` }).catch(e => { })
+    } else {
+    const success = queue.skip();
+    return interaction.reply({ content: success ? `**${queue.current.title}**, ${lang.msg83}` : lang.msg41 }).catch(e => { })
+    }
+
+} catch (e) {
+    if(client.errorLog){
+let embed = new EmbedBuilder()
+.setColor(config.embedColor)
+.setTimestamp()
+.addFields([
+        { name: "Command", value: `${interaction?.commandName}` },
+        { name: "Error", value: `${e.stack}` },
+        { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
+        { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
+        { name: "Time", value: `<t:${Math.floor(Date.now()/1000)}:R>`, inline: true },
+        { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
+        { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
+    ])
+    await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+    } else {
+    console.log(`
+    Command: ${interaction?.commandName}
+    Error: ${e}
+    User: ${interaction?.user?.tag} (${interaction?.user?.id})
+    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
+    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
+    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
+    `)
+    }
+    return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+    }
 },
 };
