fb5b5031d53a7da30421086e0a15276f5a4d5686
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-01 19:47:23 +0300

    Update save.js

--- commands/save.js
+++ commands/save.js
@@ -1,4 +1,4 @@
-const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder } = require('discord.js');
+const { ModalBuilder, TextInputBuilder, TextInputStyle, ActionRowBuilder, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
 name: "save",
@@ -9,26 +9,52 @@ run: async (client, interaction) => {
 let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id })
 lang = lang?.language || client.language
 lang = require(`../languages/${lang}.js`);
-const queue = client.player.getQueue(interaction.guild.id);
 
-if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+try {
 
-const Modal = new ModalBuilder()
-.setCustomId("playlistModal")
-.setTitle(lang.msg6)
+    const queue = client.player.getQueue(interaction.guild.id);
+    if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+    
+    const Modal = new ModalBuilder()
+    .setCustomId("playlistModal")
+    .setTitle(lang.msg6)
+    
+    const PlayList = new TextInputBuilder()
+    .setCustomId("playlist")
+    .setLabel(lang.msg7)
+    .setRequired(true)
+    .setStyle(TextInputStyle.Short)
+    
+    const PlaylistRow = new ActionRowBuilder().addComponents(PlayList);
+    Modal.addComponents(PlaylistRow)
+    await interaction.showModal(Modal).catch(e => {})
 
-const PlayList = new TextInputBuilder()
-.setCustomId("playlist")
-.setLabel(lang.msg7)
-.setRequired(true)
-.setStyle(TextInputStyle.Short)
-
-const PlaylistRow = new ActionRowBuilder().addComponents(PlayList);
-
-Modal.addComponents(PlaylistRow)
-
-await interaction.showModal(Modal).catch(e => {})
-
-
+} catch (e) {
+    if(client.errorLog){
+let embed = new EmbedBuilder()
+.setColor(config.embedColor)
+.setTimestamp()
+.addFields([
+        { name: "Command", value: `${interaction?.commandName}` },
+        { name: "Error", value: `${e.stack}` },
+        { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
+        { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
+        { name: "Time", value: `<t:${Math.floor(Date.now()/1000)}:R>`, inline: true },
+        { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
+        { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
+    ])
+    await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+    } else {
+    console.log(`
+    Command: ${interaction?.commandName}
+    Error: ${e}
+    User: ${interaction?.user?.tag} (${interaction?.user?.id})
+    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
+    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
+    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
+    `)
+    }
+    return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+    }
 },
 };
