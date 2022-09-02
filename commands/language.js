91abc2e4d95ccf22f819020357d4efe0aa094da6
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-02 10:03:04 +0300

    Update language.js

--- commands/language.js
+++ commands/language.js
@@ -81,7 +81,7 @@ try {
     } catch (e) {
         if(client.errorLog){
     let embed = new EmbedBuilder()
-    .setColor(config.embedColor)
+    .setColor(client.config.embedColor)
     .setTimestamp()
     .addFields([
             { name: "Command", value: `${interaction?.commandName}` },
