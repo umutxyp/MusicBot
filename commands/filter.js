0d54766768a33278e0f444cce46dd995ecbadbcd
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-02 10:02:42 +0300

    Update filter.js

--- commands/filter.js
+++ commands/filter.js
@@ -40,7 +40,7 @@ interaction.reply({ content: lang.msg31.replace("{filter}", filter).replace("{st
 } catch (e) {
   if(client.errorLog){
 let embed = new EmbedBuilder()
-.setColor(config.embedColor)
+.setColor(client.config.embedColor)
 .setTimestamp()
 .addFields([
       { name: "Command", value: `${interaction?.commandName}` },
