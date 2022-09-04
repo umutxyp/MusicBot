4acc824a1ef8450eb88891c3f4967a9ce12c58fd
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-04 06:43:08 +0300

    Delete events/player directory

--- events/player/connectionError.js
+++
@@ -1,8 +0,0 @@
-module.exports = async (client, queue, error) => {
-if (queue) {
-queue?.clear();
-if (queue?.metadata) {
-queue?.metadata?.send({ content: `**Connection Error:** ${error} ❌ ` }).catch(e => { })
-}
-}
-}
