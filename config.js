d14981e0b3c60ed67d4ff82c7662868ea55bdc29
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-04 06:40:49 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,35 +9,35 @@ language: "en", //en, tr
 embedColor: "ffa954", //hex color code
 errorLog: "", //write your discord error log channel id.
   
+    voteManager: { //optional
+        status: false, //true or false
+        api_key: "", //write your top.gg api key. 
+        vote_commands: ["back","channel","clear","dj","filter","loop","nowplaying","pause","play","playlist","queue","resume","save","search","skip","stop","time","volume"], //write your use by vote commands.
+        vote_url: "", //write your top.gg vote url.
+    },
+
+    playlistSettings:{
+        maxPlaylist: 10, //max playlist count
+        maxMusic: 75, //max music count
+    },
+
 opt: {
 DJ: {
 commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
 },
 
 voiceConfig: {
-leaveOnEnd: false, //If this variable is "true", the bot will leave the channel the music ends.
-autoSelfDeaf: false, //IF YOU WANT TO DEAF THE BOT, set false to true.
+leaveOnFinish: false, //If this variable is "true", the bot will leave the channel the music ends.
+leaveOnStop: false, //If this variable is "true", the bot will leave the channel when the music is stopped.
 
 leaveOnEmpty: { //The leaveOnEnd variable must be "false" to use this system.
-status: false, //If this variable is "true", the bot will leave the channel when the bot is offline.
-cooldown: 1000000, //1000 = 1 second
+status: true, //If this variable is "true", the bot will leave the channel when the bot is offline.
+cooldown: 10000000, //1000 = 1 second
 },
 
-leaveOnTimer: { //The leaveOnEnd variable must be "false" to use this system.
-status: false, //If this variable is "true", the bot will leave the channel when the bot is offline.
-cooldown: 1000000, //1000 = 1 second
-}
 },
 
 maxVol: 150, //You can specify the maximum volume level.
-loopMessage: false,
 
-discordPlayer: {
-ytdlOptions: {
-quality: 'highestaudio', //Please don't touch
-highWaterMark: 1 << 25 //Please don't touch
 }
 }
-}
-}
-
