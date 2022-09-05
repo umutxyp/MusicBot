5c958b25edfba41efd3fb3dc3d95bc8147fbf4da
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-05 04:11:28 +0300

    Update mongoDB.js

--- mongoDB.js
+++ mongoDB.js
@@ -1,51 +1,51 @@
 const { Schema, model } = require("mongoose");
 
 const musicbot = Schema({
-guildID: String,
-role: String,
-language: String,
-channels: Array
-})
+  guildID: String,
+  role: String,
+  language: String,
+  channels: Array,
+});
 
 const loop = Schema({
-userID: String,
-guildID: String,
-channelID: String,
-messageID: String
-})
+  userID: String,
+  guildID: String,
+  channelID: String,
+  messageID: String,
+});
 
 const queue = Schema({
-userID: String,
-guildID: String,
-channelID: String,
-messageID: String
-})
+  userID: String,
+  guildID: String,
+  channelID: String,
+  messageID: String,
+});
 
 const playlist = Schema({
-userID: String,
-playlist: Array,
-musics: Array
-})
+  userID: String,
+  playlist: Array,
+  musics: Array,
+});
 
 const playlist_timer = Schema({
-userID: String,
-guildID: String,
-channelID: String,
-messageID: String
-})
+  userID: String,
+  guildID: String,
+  channelID: String,
+  messageID: String,
+});
 
 const playlist_timer2 = Schema({
-userID: String,
-guildID: String,
-channelID: String,
-messageID: String
-})
+  userID: String,
+  guildID: String,
+  channelID: String,
+  messageID: String,
+});
 
 module.exports = {
-musicbot: model("musicbot", musicbot),
-loop: model("loop", loop),
-queue: model("queue", queue),
-playlist: model("playlist", playlist),
-playlist_timer: model("playlist_timer", playlist_timer),
-playlist_timer2: model("playlist_timer2", playlist_timer2)
-}
+  musicbot: model("musicbot", musicbot),
+  loop: model("loop", loop),
+  queue: model("queue", queue),
+  playlist: model("playlist", playlist),
+  playlist_timer: model("playlist_timer", playlist_timer),
+  playlist_timer2: model("playlist_timer2", playlist_timer2),
+};
