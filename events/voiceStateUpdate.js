2aa452554194570a0ff6b244941d44d34074d856
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-09-10 00:16:20 +0300

    Update voiceStateUpdate.js

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -2,6 +2,26 @@ const db = require("../mongoDB");
 module.exports = async (client, oldState, newState) => {
 const queue = client.player.getQueue(oldState.guild.id);
 if(queue || queue?.playing){
+if(client?.config?.opt?.voiceConfig?.leaveOnEmpty?.status === true){
+let lang = await db?.musicbot?.findOne({ guildID: queue?.textChannel?.guild?.id })
+lang = lang?.language || client.language
+lang = require(`../languages/${lang}.js`);
+setTimeout(async() => {
+let botChannel = oldState?.guild?.channels?.cache?.get(queue?.voice?.connection?.joinConfig?.channelId)
+if(botChannel){
+if(botChannel.id == oldState.channelId)
+if(botChannel?.members?.find(x => x == client?.user?.id)){
+if(botChannel?.members?.size == 1){
+await queue?.textChannel?.send({ content: `${lang.msg15}` }).catch(e => { })
+if(queue || queue?.playing){
+return queue?.stop(oldState.guild.id)
+}
+}
+}
+}
+}, client?.config?.opt?.voiceConfig?.leaveOnEmpty?.cooldown || 60000)
+}
+
 if(newState.id === client.user.id){
 let lang = await db?.musicbot?.findOne({ guildID: queue?.textChannel?.guild?.id })
 lang = lang?.language || client.language
