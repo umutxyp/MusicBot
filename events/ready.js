f9eb42b9f57a5414bcbb89c8eb5ff6d9e761b046
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-10-02 20:45:45 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,74 +1,67 @@
 const config = require("../config.js");
+const { ActivityType  } = require("discord.js")
 module.exports = async (client) => {
-  let lang = client.language;
-  lang = require(`../languages/${lang}.js`);
+let lang = client.language
+lang = require(`../languages/${lang}.js`);
 
-  if (config.mongodbURL || process.env.MONGO) {
-    const mongoose = require("mongoose");
-    mongoose
-      .connect(config.mongodbURL || process.env.MONGO, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(async () => {
-        console.log(`Connected MongoDB`);
+if (config.mongodbURL || process.env.MONGO) {
+const mongoose = require("mongoose")
+mongoose.connect(config.mongodbURL || process.env.MONGO, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+}).then(async () => {
+console.log(`Connected MongoDB`)
 
-        const { REST } = require("@discordjs/rest");
-        const { Routes } = require("discord-api-types/v10");
-        const rest = new REST({ version: "10" }).setToken(
-          config.TOKEN || process.env.TOKEN
-        );
-        (async () => {
-          try {
-            await rest.put(Routes.applicationCommands(client.user.id), {
-              body: await client.commands,
-            });
-            console.log(lang.loadslash);
-          } catch (err) {
-            console.log(lang.error3 + err);
-          }
-        })();
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v10");
+const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
+(async () => {
+try {
+await rest.put(Routes.applicationCommands(client.user.id), {
+body: await client.commands,
+});
+console.log(lang.loadslash)
+} catch (err) {
+console.log(lang.error3 + err);
+}
+})();
 
-        console.log(client.user.username + lang.ready);
+console.log(client.user.username + lang.ready);
+  
+setInterval(() => client.user.setActivity({ name: `${config.status}`, type: ActivityType.Listening }), 30000);
+client.errorLog = client?.channels?.cache?.get(config.errorLog) ? client?.channels?.cache?.get(config.errorLog) : undefined;
 
-        client.user.setStatus("ONLINE");
-        client.user.setActivity(config.status);
-        client.errorLog = client.channels.cache.get(config.errorLog)
-          ? client.channels.cache.get(config.errorLog)
-          : undefined;
+setTimeout(async () => {
+const db = require("../mongoDB");
+await db.loop.deleteOne()
+await db.queue.deleteOne()
+await db.playlist_timer.deleteOne()
+await db.playlist_timer2.deleteOne()
+}, 5000)
 
-        setTimeout(async () => {
-          const db = require("../mongoDB");
-          await db.loop.deleteOne();
-          await db.queue.deleteOne();
-          await db.playlist_timer.deleteOne();
-          await db.playlist_timer2.deleteOne();
-        }, 5000);
-      })
-      .catch((err) => {
-        console.log("\nMongoDB Error: " + err + "\n\n" + lang.error4);
-      });
-  } else {
-    console.log(lang.error4);
-  }
+}).catch((err) => {
+console.log("\nMongoDB Error: " + err + "\n\n" + lang.error4)
+})
 
-  try {
-    function users_fetch() {
-      client.guilds.cache.forEach(async (guild) => {
-        await guild.members.fetch();
-      });
-    }
-    setInterval(users_fetch, 10000000);
-  } catch (err) {
-    return;
-  }
+} else {
+console.log(lang.error4)
+}
 
-  if (
-    client.config.voteManager.status === true &&
-    client.config.voteManager.api_key
-  ) {
-    const { AutoPoster } = require("topgg-autoposter");
-    const ap = AutoPoster(client.config.voteManager.api_key, client);
-    ap.on("posted", () => {});
-  }
-};
+try {
+client.guilds.cache.map(async guild => {
+await guild.members.fetch()
+})
+
+} catch (err) {
+return
+}
+
+
+if(client.config.voteManager.status === true && client.config.voteManager.api_key){
+const { AutoPoster } = require('topgg-autoposter')
+const ap = AutoPoster(client.config.voteManager.api_key, client)
+ap.on('posted', () => {
+})
+}
+
+}
