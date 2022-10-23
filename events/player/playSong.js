d5b06b9b6c4d86bdfaddda3a44cb4ad51dfe8874
hololight <uselesszamir@gmail.com>
2022-10-23 13:05:54 +0300

    Added embed message to playing song (no need to type /nowplaying, but sometimes you need cause I can't do everything, sorry)

--- events/player/playSong.js
+++ events/player/playSong.js
@@ -1,12 +1,37 @@
 const db = require("../../mongoDB");
+const {EmbedBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder} = require("discord.js");
 module.exports = async (client, queue, song) => {
-let lang = await db?.musicbot?.findOne({ guildID: queue?.textChannel?.guild?.id })
-lang = lang?.language || client.language
-lang = require(`../../languages/${lang}.js`);
-if (queue) {
-if (!client.config.opt.loopMessage && queue?.repeatMode !== 0) return;
-if (queue?.textChannel) {
-queue?.textChannel?.send({ content: lang.msg13.replace("{track?.title}", song?.name).replace("{queue?.connection.channel.name}", `<#${queue.voice.connection.joinConfig.channelId}>`) }).catch(e => { });
-}
-}
+    let lang = await db?.musicbot?.findOne({guildID: queue?.textChannel?.guild?.id})
+    lang = lang?.language || client.language
+    lang = require(`../../languages/${lang}.js`);
+    if (queue) {
+        if (!client.config.opt.loopMessage && queue?.repeatMode !== 0) return;
+        const embed = new EmbedBuilder();
+        embed.setColor(client.config.embedColor);
+        embed.setThumbnail(song.thumbnail);
+        embed.setTitle("Now playing - " + song.name)
+        embed.setDescription(`> Audio \`%${queue.volume}\`
+> Duration \`${song.formattedDuration}\`
+> URL: **${song.url}**
+> Loop Mode \`${queue.repeatMode ? (queue.repeatMode === 2 ? 'All Queue' : 'This Song') : 'Off'}\`
+> Filter: \`${queue.filters.names.join(', ') || 'Off'}\`
+> By: <@${song.user.id}>`);
+        embed.setTimestamp();
+
+        const row = new ActionRowBuilder().addComponents(
+            new ButtonBuilder()
+                .setLabel(lang.msg47)
+                .setCustomId('saveTrack')
+                .setStyle(ButtonStyle.Success),
+            new ButtonBuilder()
+                .setLabel(lang.msg134)
+                .setCustomId('skip')
+                .setStyle(ButtonStyle.Success)
+        );
+
+        if (queue?.textChannel) {
+            queue?.textChannel?.send({embeds: [embed], components: [row]}).catch(() => {
+            });
+        }
+    }
 }
