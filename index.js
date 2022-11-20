745faa012a1dc339f40daf93a15b0e0ed4bba5af
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-11-20 23:00:50 +0300

    added shard system.

---
+++ index.js
@@ -0,0 +1,14 @@
+const config = require('./config.js');
+
+if(config.shardManager.shardStatus == true){
+
+const { ShardingManager } = require('discord.js');
+const manager = new ShardingManager('./bot.js', { token: config.TOKEN || process.env.TOKEN });
+manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+manager.spawn();
+
+} else {
+
+require("./bot.js")
+
+}
