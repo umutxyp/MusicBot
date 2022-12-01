534843fd1fefa1e25bae21754dae68d2b0db72be
Darkzx <75396448+Darkzx0@users.noreply.github.com>
2022-12-01 14:17:40 -0400

    Autoplay

---
+++ commands/autoplay.js
@@ -0,0 +1,47 @@
+const { EmbedBuilder } = require("discord.js")
+const config = require("../config.js");
+const db = require("../mongoDB");
+module.exports = {
+  name: "autoplay",
+  description: "Toggle the autoplay of the queue.",
+  options: [],
+  permissions: "0x0000000000000800",
+  run: async (client, interaction) => {
+    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id }).catch(e => {})
+    lang = lang?.language || client.language
+    lang = require(`../languages/${lang}.js`);
+    try {
+        const queue = client.player.getQueue(interaction.guild.id);
+        if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+        queue.toggleAutoplay()
+        interaction.reply(queue.autoplay ? lang.msg136 : lang.msg137)
+    
+    } catch (e) {
+      if (client.errorLog) {
+        let embed = new EmbedBuilder()
+          .setColor(client.config.embedColor)
+          .setTimestamp()
+          .addFields([
+            { name: "Command", value: `${interaction?.commandName}` },
+            { name: "Error", value: `${e.stack}` },
+            { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
+            { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
+            { name: "Time", value: `<t:${Math.floor(Date.now() / 1000)}:R>`, inline: true },
+            { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
+            { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
+          ])
+        await client.errorLog.send({ embeds: [embed] }).catch(e => { })
+      } else {
+        console.log(`
+    Command: ${interaction?.commandName}
+    Error: ${e}
+    User: ${interaction?.user?.tag} (${interaction?.user?.id})
+    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
+    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
+    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
+    `)
+      }
+      return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
+    }
+  },
+};
--- languages/pt.js
+++ languages/pt.js
@@ -147,6 +147,8 @@ const language = {
    msg132: "A música não está pausada.",
    msg133: "Eu misturei a ordem da playlist.",
    msg134: "Uso incorreto. Exemplo: `5:50` | `1:12:43`",
-   msg135: "O tempo de reprodução foi definido para {queue.formattedCurrentTime} com sucesso."
+   msg135: "O tempo de reprodução foi definido para {queue.formattedCurrentTime} com sucesso.",
+   msg136: "O autoplay foi ligado com sucesso",
+   msg137: "O autoplay foi desligado com sucesso",
 }
 module.exports = language;
