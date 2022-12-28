4bf233de5f588c2a3acdd3570fd4befc70d55ead
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-12-28 22:59:08 +0300

    Update autoplay.js

--- commands/autoplay.js
+++ commands/autoplay.js
@@ -6,14 +6,14 @@ module.exports = {
   options: [],
   permissions: "0x0000000000000800",
   run: async (client, interaction) => {
-    let lang = await db?.musicbot?.findOne({ guildID: interaction.guild.id }).catch(e => {})
+    let lang = await db?.musicbot?.findOne({ guildID: interaction?.guild?.id }).catch(e => {})
     lang = lang?.language || client.language
     lang = require(`../languages/${lang}.js`);
     try {
-        const queue = client.player.getQueue(interaction.guild.id);
-        if (!queue || !queue.playing) return interaction.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
-        queue.toggleAutoplay()
-        interaction.reply(queue.autoplay ? lang.msg136 : lang.msg137)
+        const queue = client?.player?.getQueue(interaction?.guild?.id);
+        if (!queue || !queue?.playing) return interaction?.reply({ content: lang.msg5, ephemeral: true }).catch(e => { })
+        queue?.toggleAutoplay()
+        interaction?.reply(queue?.autoplay ? lang.msg136 : lang.msg137)
     
     } catch (e) {
       if (client.errorLog) {
