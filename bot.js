cd546de70ef29cf5882268a08236664d556e4996
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-12-28 23:13:25 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ const { Client, GatewayIntentBits, Partials } = require("discord.js");
 const { DisTube } = require("distube");
 const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
+const { DeezerPlugin } = require("@distube/deezer");
 const { YtDlpPlugin } = require("@distube/yt-dlp");
 const config = require("./config");
 const fs = require("fs");
@@ -27,11 +28,10 @@ client.player = new DisTube(client, {
   emitAddSongWhenCreatingQueue: false,
   emitAddListWhenCreatingQueue: false,
   plugins: [
-    new SpotifyPlugin({
-      emitEventsAfterFetching: true,
-    }),
+    new SpotifyPlugin(),
     new SoundCloudPlugin(),
     new YtDlpPlugin(),
+    new DeezerPlugin()
   ],
 });
 
