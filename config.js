a645a47ad156b8f1ce94d142eca9d2bd206acdd3
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2022-12-31 16:20:51 +0300

    Update config.js

--- config.js
+++ config.js
@@ -9,23 +9,28 @@ commandsDir: './commands', //Please don't touch
 language: "en", //en, tr, nl, pt, fr, ar, zh_TW, it
 embedColor: "ffa954", //hex color code
 errorLog: "", //write your discord error log channel id.
-sponsor: "https://awmbilisim.com",  //write your discord sponsor url.
-  
-    voteManager: { //optional
-        status: false, //true or false
-        api_key: "", //write your top.gg api key. 
-        vote_commands: ["back","channel","clear","dj","filter","loop","nowplaying","pause","play","playlist","queue","resume","save","search","skip","stop","time","volume"], //write your use by vote commands.
-        vote_url: "", //write your top.gg vote url.
-    },
-  
-   shardManager:{
-     shardStatus: false //If your bot exists on more than 1000 servers, change this part to true.
-   },
 
-    playlistSettings:{
-        maxPlaylist: 10, //max playlist count
-        maxMusic: 75, //max music count
-    },
+
+sponsor: {
+status: true, //true or false
+url: "https://awmbilisim.com", //write your discord sponsor url.
+},
+
+voteManager: { //optional
+status: false, //true or false
+api_key: "", //write your top.gg api key. 
+vote_commands: ["back","channel","clear","dj","filter","loop","nowplaying","pause","play","playlist","queue","resume","save","search","skip","stop","time","volume"], //write your use by vote commands.
+vote_url: "", //write your top.gg vote url.
+},
+
+shardManager:{
+shardStatus: false //If your bot exists on more than 1000 servers, change this part to true.
+},
+
+playlistSettings:{
+maxPlaylist: 10, //max playlist count
+maxMusic: 75, //max music count
+},
 
 opt: {
 DJ: {
