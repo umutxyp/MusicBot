e79cf116e5b188ce587e753a0dfecf79632a0835
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2023-01-07 19:39:55 +0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -203,38 +203,7 @@ break
 }
 }
 } catch (e) {
-    if(client?.errorLog){
-let embed = new EmbedBuilder()
-.setColor(config?.embedColor)
-.setTimestamp()
-.addFields([
-        { name: "Command", value: `${interaction?.commandName}` },
-        { name: "Error", value: `${e?.stack}` },
-        { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
-        { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
-        { name: "Time", value: `<t:${Math.floor(Date.now()/1000)}:R>`, inline: true },
-        { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
-        { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
-    ])
-    
-    return client.shard.broadcastEval(async (c, { channelId }) => {
-        const channel = c.channels.cache.get(channelId);
-        if (channel) {
-            await channel?.send({ embeds: [embed] }).catch(e => { })
-            return true;
-        }
-        return false;
-    }, { context: { channelId: client?.errorLog } })
-    } else {
-    console.log(`
-    Command: ${interaction?.commandName}
-    Error: ${e}
-    User: ${interaction?.user?.tag} (${interaction?.user?.id})
-    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
-    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
-    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
-    `)
-    }
-    return interaction?.reply({ content: `${lang?.error7}`, ephemeral: true }).catch(e => { })
+    const errorNotifer = require("../functions.js")
+   errorNotifer(client, interaction, e, lang)
     }
 }
