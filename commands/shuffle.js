645a55e61accdfe29d63b3b54e0ac8d2e7389685
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2023-01-07 20:05:15 +0300

    Update shuffle.js

--- commands/shuffle.js
+++ commands/shuffle.js
@@ -1,4 +1,4 @@
-const { EmbedBuilder, version } = require("discord.js")
+const { EmbedBuilder } = require("discord.js")
 const config = require("../config.js");
 const db = require("../mongoDB");
 module.exports = {
@@ -20,43 +20,9 @@ module.exports = {
         } catch(err) {
         return interaction.reply({ content: `**${err}**` }).catch(e => { })
         }
-    } catch (e) {
-      if (client.errorLog) {
-        let embed = new EmbedBuilder()
-          .setColor(client.config.embedColor)
-          .setTimestamp()
-          .addFields([
-            { name: "Command", value: `${interaction?.commandName}` },
-            { name: "Error", value: `${e.stack}` },
-            { name: "User", value: `${interaction?.user?.tag} \`(${interaction?.user?.id})\``, inline: true },
-            { name: "Guild", value: `${interaction?.guild?.name} \`(${interaction?.guild?.id})\``, inline: true },
-            { name: "Time", value: `<t:${Math.floor(Date.now() / 1000)}:R>`, inline: true },
-            { name: "Command Usage Channel", value: `${interaction?.channel?.name} \`(${interaction?.channel?.id})\``, inline: true },
-            { name: "User Voice Channel", value: `${interaction?.member?.voice?.channel?.name} \`(${interaction?.member?.voice?.channel?.id})\``, inline: true },
-          ])
-          if(client.shard){
-            return client.shard.broadcastEval(async (c, { channelId }) => {
-              const channel = c.channels.cache.get(channelId);
-              if (channel) {
-                  await channel?.send({ embeds: [embed] }).catch(e => { })
-                  return true;
-              }
-              return false;
-          }, { context: { channelId: client?.errorLog } })
-        } else {
-          client.channels.cache.get(client?.errorLog)?.send({ embeds: [embed] }).catch(e => { })
+      } catch (e) {
+        const errorNotifer = require("../functions.js")
+       errorNotifer(client, interaction, e, lang)
         }
-      } else {
-        console.log(`
-    Command: ${interaction?.commandName}
-    Error: ${e}
-    User: ${interaction?.user?.tag} (${interaction?.user?.id})
-    Guild: ${interaction?.guild?.name} (${interaction?.guild?.id})
-    Command Usage Channel: ${interaction?.channel?.name} (${interaction?.channel?.id})
-    User Voice Channel: ${interaction?.member?.voice?.channel?.name} (${interaction?.member?.voice?.channel?.id})
-    `)
-      }
-      return interaction.reply({ content: `${lang.error7}\n\`${e}\``, ephemeral: true }).catch(e => { })
-    }
   },
 };
