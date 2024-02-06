69300179bbccc4dc0a72b12c8d7823577e58b549
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2024-02-06 18:54:58 +0300

    Delete events directory

--- events/player/error.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (client, textChannel, e) => {
-if (textChannel){
-   return textChannel?.send(`**An error encountered:** ${e.toString().slice(0, 1974)}`)
-}
-}
--- events/player/finish.js
+++
@@ -1,9 +0,0 @@
-const db = require("../../mongoDB");
-module.exports = async (client, queue) => {
-let lang = await db?.musicbot?.findOne({ guildID: queue?.textChannel?.guild?.id })
-lang = lang?.language || client.language
-lang = require(`../../languages/${lang}.js`);
-if (queue?.textChannel) {
-queue?.textChannel?.send({ content: `${lang.msg14}` }).catch(e => { })
-}
-}
