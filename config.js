9c1e2fdae491284f2a5634b5457d79d30c5ce642
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2025-03-11 21:58:01 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: "",
+    TOKENS: [""],
     ownerID: [""], //write your discord user id. example: ["id"] or ["id1","id2"]
     botInvite: "", //write your discord bot invite.
     supportServer: "", //write your discord bot support server invite.
