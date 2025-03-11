e3da3f7e4847746f1bf437daf0d7dff39279430c
Umut Bayraktar <69085038+umutxyp@users.noreply.github.com>
2025-03-11 21:53:24 +0300

    Update SETUP.bat

--- SETUP.bat
+++ SETUP.bat
@@ -1,6 +1,58 @@
-echo off
-color 1
-cls
-:a
-npm install
-goto a
+@echo off
+echo.
+echo ===============================
+echo  Local dependencies setup.
+echo ===============================
+echo.
+
+npm install ytdl-core@^4.11.5
+npm install topgg-autoposter@^2.0.2
+npm install sapphire@^0.0.3
+npm install mongoose@^8.10.1
+npm install libsodium-wrappers@^0.7.15
+npm install ffmpeg-static@^5.2.0
+npm install ffmpeg@^0.0.4
+npm install express@^4.21.2
+npm install distube@^4.0.4
+npm install discord.js@^14.18.0
+npm install @top-gg/sdk@^3.1.6
+npm install @distube/ytdl-core@^4.15.1
+npm install @distube/ytdl@^2.0.6
+npm install @distube/yt-dlp@^1.1.3
+npm install @distube/spotify@^1.5.1
+npm install @distube/soundcloud@^1.3.3
+npm install @discordjs/voice@^0.18.0
+npm install @discordjs/rest@^2.4.3
+npm install @discordjs/opus@^0.10.0
+npm install @discordjs/builders@^1.10.1
+
+echo.
+echo ===============================
+echo  Global dependencies setup.
+echo ===============================
+echo.
+
+npm install -g ytdl-core@^4.11.5
+npm install -g topgg-autoposter@^2.0.2
+npm install -g sapphire@^0.0.3
+npm install -g mongoose@^8.10.1
+npm install -g libsodium-wrappers@^0.7.15
+npm install -g ffmpeg-static@^5.2.0
+npm install -g ffmpeg@^0.0.4
+npm install -g express@^4.21.2
+npm install -g distube@^4.0.4
+npm install -g discord.js@^14.18.0
+npm install -g @top-gg/sdk@^3.1.6
+npm install -g @distube/ytdl-core@^4.15.1
+npm install -g @distube/ytdl@^2.0.6
+npm install -g @distube/yt-dlp@^1.1.3
+npm install -g @distube/spotify@^1.5.1
+npm install -g @distube/soundcloud@^1.3.3
+npm install -g @discordjs/voice@^0.18.0
+npm install -g @discordjs/rest@^2.4.3
+npm install -g @discordjs/opus@^0.10.0
+npm install -g @discordjs/builders@^1.10.1
+
+echo.
+echo Setup successfuly.
+pause
